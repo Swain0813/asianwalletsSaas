a1d32b90f024ab5a9ee6d87ff096c96ebb1cc91a
yangxu <yangxu@asianwallets.com>
2019-11-21 14:34:13 +0800

    1

---
+++ asianwallets-eureka/src/test/java/com/asianwallets/eureka/AsianwalletsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asianwallets.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsianwalletsEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
