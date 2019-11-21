f87ce7dc1fd730877c723154102ea0c56154cac3
yangxu <yangxu@asianwallets.com>
2019-11-21 16:24:35 +0800

    1

---
+++ asianwallets-permissions/src/test/java/com/asianwallets/permissions/PermissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asianwallets.permissions;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
