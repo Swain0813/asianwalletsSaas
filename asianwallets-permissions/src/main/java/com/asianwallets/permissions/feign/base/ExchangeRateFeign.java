70d697ad3308b7425240cfd5a9cc742c38e9d1dd
DESKTOP-311TI2U\yangs <yangshanlong@asianwallets.com>
2019-11-25 14:11:43 +0800

    更新

--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/ExchangeRateFeign.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/ExchangeRateFeign.java
@@ -2,6 +2,7 @@ package com.asianwallets.permissions.feign.base;
 import com.asianwallets.common.dto.ExchangeRateDTO;
 import com.asianwallets.common.response.BaseResponse;
 import com.asianwallets.permissions.feign.base.impl.ExchangeRateFeignImpl;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,28 +16,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "asianwallets-base", fallback = ExchangeRateFeignImpl.class)
 public interface ExchangeRateFeign {
 
-    /**
-     * 添加汇率信息
-     * @param exchangeRateDTO
-     * @return
-     */
+    @ApiOperation(value = "添加汇率信息")
     @PostMapping("/exchangeRate/addExchangeRate")
     BaseResponse addExchangeRate(@RequestBody @ApiParam ExchangeRateDTO exchangeRateDTO);
 
-    /**
-     * 禁用汇率信息
-     * @param id
-     * @return
-     */
+    @ApiOperation(value = "禁用汇率信息")
     @GetMapping("/exchangeRate/banExchangeRate")
     BaseResponse banExchangeRate(@RequestParam("id") @ApiParam String id);
 
 
-    /**
-     * 分页查询汇率信息
-     * @param exchangeRateDTO
-     * @return
-     */
+    @ApiOperation(value = "分页查询汇率信息")
     @PostMapping("/exchangeRate/getByMultipleConditions")
     BaseResponse getByMultipleConditions(@RequestBody @ApiParam ExchangeRateDTO exchangeRateDTO);
 
--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/HolidaysFeign.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/HolidaysFeign.java
@@ -4,6 +4,7 @@ import com.asianwallets.common.dto.HolidaysDTO;
 import com.asianwallets.common.entity.Holidays;
 import com.asianwallets.common.response.BaseResponse;
 import com.asianwallets.permissions.feign.base.impl.HolidaysFeignImpl;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,35 +18,19 @@ import java.util.List;
 @FeignClient(value = "asianwallets-base", fallback = HolidaysFeignImpl.class)
 public interface HolidaysFeign {
 
-    /**
-     * 添加节假日信息
-     * @param holidaysDTO
-     * @return
-     */
+    @ApiOperation(value = "添加节假日信息")
     @PostMapping("/holidays/addHolidays")
     BaseResponse addHolidays(@RequestBody @ApiParam HolidaysDTO holidaysDTO);
 
-    /**
-     * 禁用节假日信息
-     * @param holidaysDTO
-     * @return
-     */
+    @ApiOperation(value = "禁用节假日信息")
     @PostMapping("/holidays/banHolidays")
     BaseResponse banHolidays(@RequestBody @ApiParam HolidaysDTO holidaysDTO);
 
-    /**
-     * 分页多条件查询节假日信息
-     * @param holidaysDTO
-     * @return
-     */
+    @ApiOperation(value = "分页多条件查询节假日信息")
     @PostMapping("/holidays/getByMultipleConditions")
     BaseResponse getByMultipleConditions(@RequestBody @ApiParam HolidaysDTO holidaysDTO);
 
-    /**
-     * 导入节假日信息
-     * @param list
-     * @return
-     */
+    @ApiOperation(value = "导入节假日信息")
     @PostMapping(value = "/holidays/uploadFiles")
     BaseResponse uploadFiles(@RequestBody @ApiParam List<Holidays> list);
 }
