fb70739081fa6c367d202371e2c15a68efc302cc
yangxu <yangxu@asianwallets.com>
2019-11-25 14:29:13 +0800

    1

---
+++ asianwallets-base/src/main/java/com/asianwallets/base/dao/MerchantMapper.java
@@ -0,0 +1,18 @@
+package com.asianwallets.base.dao;
+
+import com.asianwallets.common.base. BaseMapper;
+import com.asianwallets.common.entity.Merchant;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author yx
+ * @since 2019-11-25
+ */
+@Repository
+public interface MerchantMapper extends  BaseMapper<Merchant> {
+
+}
---
+++ asianwallets-base/src/main/java/com/asianwallets/base/service/MerchantService.java
@@ -0,0 +1,16 @@
+package com.asianwallets.base.service;
+
+import com.asianwallets.common.base.BaseService;
+import com.asianwallets.common.entity.Merchant;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author yx
+ * @since 2019-11-25
+ */
+public interface MerchantService extends BaseService<Merchant> {
+
+}
