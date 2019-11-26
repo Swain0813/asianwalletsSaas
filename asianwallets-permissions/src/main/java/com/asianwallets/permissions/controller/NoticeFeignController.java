5f9be2ae62bd1ab35376c2a109778178474ca774
DESKTOP-311TI2U\yangs <yangshanlong@asianwallets.com>
2019-11-26 13:07:08 +0800

    修改

--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/controller/NoticeFeignController.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/controller/NoticeFeignController.java
@@ -4,8 +4,7 @@ import com.asianwallets.common.base.BaseController;
 import com.asianwallets.common.constant.AsianWalletConstant;
 import com.asianwallets.common.dto.NoticeDTO;
 import com.asianwallets.common.response.BaseResponse;
-import com.asianwallets.common.response.ResultUtil;
-import com.asianwallets.permissions.service.NoticeService;
+import com.asianwallets.permissions.feign.base.NoticeFeign;
 import com.asianwallets.permissions.service.OperationLogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -17,15 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 公告相关业务的服务
- **/
+ * 公告模块
+ */
 @RestController
-@Api(description ="公告相关接口服务")
+@Api(description = "公告管理接口")
 @RequestMapping("/notice")
-public class NoticeController extends BaseController {
+public class NoticeFeignController extends BaseController {
 
     @Autowired
-    private NoticeService noticeService;
+    private NoticeFeign noticeFeign;
 
     /**
      * 操作日志模块
@@ -35,37 +34,38 @@ public class NoticeController extends BaseController {
 
     @ApiOperation(value = "添加公告信息")
     @PostMapping("/addNotice")
-    public BaseResponse addNotice(@RequestBody @ApiParam NoticeDTO noticeDTO){
+    public BaseResponse addNotice(@RequestBody @ApiParam NoticeDTO noticeDTO) {
         //添加操作日志
         operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.ADD, JSON.toJSONString(noticeDTO),
                 "添加公告信息"));
-        return ResultUtil.success(noticeService.addNotice(this.getSysUserVO().getUsername(),noticeDTO));
+        return noticeFeign.addNotice(noticeDTO);
     }
 
     @ApiOperation(value = "修改公告信息")
     @PostMapping("/updateNotice")
-    public BaseResponse updateNotice(@RequestBody @ApiParam NoticeDTO noticeDTO){
+    public BaseResponse updateNotice(@RequestBody @ApiParam NoticeDTO noticeDTO) {
         //添加操作日志
         operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.UPDATE, JSON.toJSONString(noticeDTO),
                 "修改公告信息"));
-      return ResultUtil.success(noticeService.updateNotice(this.getSysUserVO().getUsername(),noticeDTO));
+        return noticeFeign.updateNotice(noticeDTO);
     }
 
     @ApiOperation(value = "查询所有公告信息")
     @PostMapping("/pageNotice")
-    public BaseResponse pageNotice(@RequestBody @ApiParam NoticeDTO noticeDTO){
+    public BaseResponse pageNotice(@RequestBody @ApiParam NoticeDTO noticeDTO) {
         //添加操作日志
         operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(noticeDTO),
                 "查询所有公告信息"));
-        return ResultUtil.success(noticeService.pageNotice(noticeDTO));
+        return noticeFeign.pageNotice(noticeDTO);
     }
 
-    @ApiOperation(value = "根据公告类别,机构编号以及语言查询公告信息")
+    @ApiOperation(value = "根据语言和公告类别查询公告信息")
     @PostMapping("/pageNoticeByLanguageAndCategory")
-    public BaseResponse pageNoticeByLanguageAndCategory(@RequestBody @ApiParam NoticeDTO noticeDTO){
+    public BaseResponse pageNoticeByLanguageAndCategory(@RequestBody @ApiParam NoticeDTO noticeDTO) {
         //添加操作日志
         operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(noticeDTO),
-                "根据公告类别,机构编号以及语言查询公告信息"));
-        return ResultUtil.success(noticeService.pageNoticeByLanguageAndCategory(noticeDTO));
+                "根据语言和公告类别查询公告信息"));
+        return noticeFeign.pageNoticeByLanguageAndCategory(noticeDTO);
     }
+
 }
---
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/NoticeFeign.java
@@ -0,0 +1,51 @@
+package com.asianwallets.permissions.feign.base;
+
+import com.asianwallets.common.dto.NoticeDTO;
+import com.asianwallets.common.response.BaseResponse;
+import com.asianwallets.permissions.feign.base.impl.NoticeFeignImpl;
+import io.swagger.annotations.ApiParam;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * 公告模块Feign端
+ */
+@FeignClient(value = "asianwallets-base", fallback = NoticeFeignImpl.class)
+public interface NoticeFeign {
+    /**
+     * 添加公告信息
+     *
+     * @param noticeDTO
+     * @return
+     */
+    @PostMapping("/notice/addNotice")
+    BaseResponse addNotice(@RequestBody @ApiParam NoticeDTO noticeDTO);
+
+    /**
+     * 修改公告信息
+     *
+     * @param noticeDTO
+     * @return
+     */
+    @PostMapping("/notice/updateNotice")
+    BaseResponse updateNotice(@RequestBody @ApiParam NoticeDTO noticeDTO);
+
+    /**
+     * 查询所有公告信息
+     *
+     * @param noticeDTO
+     * @return
+     */
+    @PostMapping("/notice/pageNotice")
+    BaseResponse pageNotice(@RequestBody @ApiParam NoticeDTO noticeDTO);
+
+    /**
+     * 根据公告类别,机构编号以及语言查询公告信息
+     *
+     * @param noticeDTO
+     * @return
+     */
+    @PostMapping("/notice/pageNoticeByLanguageAndCategory")
+    BaseResponse pageNoticeByLanguageAndCategory(@RequestBody @ApiParam NoticeDTO noticeDTO);
+}
