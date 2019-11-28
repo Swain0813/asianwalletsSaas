20c3752c2c1cf91580df29918a7a8913d386da92
XuWenQi <xuwq@asianwallets.com>
2019-11-28 11:35:59 +0800

    增加权限

---
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/dto/SecondMenuDto.java
@@ -0,0 +1,27 @@
+package com.asianwallets.permissions.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@ApiModel(value = "二级权限输入实体", description = "二级权限输入实体")
+public class SecondMenuDto {
+
+    @ApiModelProperty(value = "英文名称")
+    private String eName;
+
+    @ApiModelProperty(value = "中文名称")
+    private String cName;
+
+    @ApiModelProperty(value = "描述")
+    private String description;
+
+    @ApiModelProperty(value = "权限类型")
+    private Integer permissionType;
+
+    @ApiModelProperty(value = "三级权限输入实体集合")
+    private List<ThreeMenuDto> threeMenuDtoList;
+}
