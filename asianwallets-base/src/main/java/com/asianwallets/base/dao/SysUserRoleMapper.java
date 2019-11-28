36afa2f241dc8a517aa18f1fc023ddcf0a75649c
yangxu <yangxu@asianwallets.com>
2019-11-28 18:29:27 +0800

    1

--- asianwallets-base/src/main/java/com/asianwallets/base/dao/SysUserRoleMapper.java
+++ asianwallets-base/src/main/java/com/asianwallets/base/dao/SysUserRoleMapper.java
@@ -8,18 +8,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SysUserRoleMapper extends BaseMapper<SysUserRole> {
 
-    @Select("select id from sys_role where role_name like '%机构管理员%'")
+    @Select("select id from sys_role where role_name like '机构管理员%'")
     String getInstitutionRoleId();
 
-    @Select("select id from sys_role where role_name like '%POS机管理员%'")
+    @Select("select id from sys_role where role_name like 'POS机管理员%'")
     String getPOSRoleId();
 
-    @Select("select id from sys_role where role_name like '%代理管理员%'")
+    @Select("select id from sys_role where role_name like '代理管理员%'")
     String getAgencyRoleId();
 
-    @Select("select id from sys_role where role_name like '%商户管理员%'")
+    @Select("select id from sys_role where role_name like '商户管理员%'")
     String getMerchantRoleId();
 
-    @Select("select id from sys_role where role_name like '%集团商户管理员%'")
+    @Select("select id from sys_role where role_name like '集团商户管理员%'")
     String getGroupRoleId();
 }
