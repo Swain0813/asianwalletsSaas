666e048ad80616dbaae7680530abe289f90b1322
DESKTOP-311TI2U\yangs <yangshanlong@asianwallets.com>
2019-11-28 11:41:58 +0800

    操作日志的追加

--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/controller/InstitutionFeignController.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/controller/InstitutionFeignController.java
@@ -1,9 +1,12 @@
 package com.asianwallets.permissions.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.asianwallets.common.base.BaseController;
+import com.asianwallets.common.constant.AsianWalletConstant;
 import com.asianwallets.common.dto.InstitutionDTO;
 import com.asianwallets.common.response.BaseResponse;
 import com.asianwallets.permissions.feign.base.InstitutionFeign;
+import com.asianwallets.permissions.service.OperationLogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -23,10 +26,16 @@ public class InstitutionFeignController extends BaseController {
     @Autowired
     private InstitutionFeign institutionFeign;
 
+    @Autowired
+    private OperationLogService operationLogService;
+
 
     @ApiOperation(value = "添加机构")
     @PostMapping("addInstitution")
     public BaseResponse addInstitution(@RequestBody @ApiParam InstitutionDTO institutionDTO) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.ADD, JSON.toJSONString(institutionDTO),
+                "添加机构"));
         return institutionFeign.addInstitution(institutionDTO);
     }
 
@@ -34,24 +43,36 @@ public class InstitutionFeignController extends BaseController {
     @ApiOperation(value = "修改机构")
     @PostMapping("updateInstitution")
     public BaseResponse updateInstitution(@RequestBody @ApiParam InstitutionDTO institutionDTO) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.UPDATE, JSON.toJSONString(institutionDTO),
+                "修改机构"));
         return institutionFeign.updateInstitution(institutionDTO);
     }
 
     @ApiOperation(value = "分页查询机构信息列表")
     @PostMapping("/pageFindInstitution")
     public BaseResponse pageFindInstitution(@RequestBody @ApiParam InstitutionDTO institutionDTO) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(institutionDTO),
+                "分页查询机构信息列表"));
         return institutionFeign.pageFindInstitution(institutionDTO);
     }
 
     @ApiOperation(value = "分页查询机构审核信息列表")
     @PostMapping("/pageFindInstitutionAudit")
     public BaseResponse pageFindInstitutionAudit(@RequestBody @ApiParam InstitutionDTO institutionDTO) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(institutionDTO),
+                "分页查询机构审核信息列表"));
         return institutionFeign.pageFindInstitutionAudit(institutionDTO);
     }
 
     @ApiOperation(value = "根据机构Id查询机构信息详情")
     @GetMapping("/getInstitutionInfo")
     public BaseResponse getInstitutionInfo(@RequestParam @ApiParam String id) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(id),
+                "根据机构Id查询机构信息详情"));
         return institutionFeign.getInstitutionInfo(id);
     }
 
@@ -59,12 +80,18 @@ public class InstitutionFeignController extends BaseController {
     @ApiOperation(value = "根据机构Id查询机构审核信息详情")
     @GetMapping("/getInstitutionInfoAudit")
     public BaseResponse getInstitutionInfoAudit(String id) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(id),
+                "根据机构Id查询机构审核信息详情"));
         return institutionFeign.getInstitutionInfoAudit(id);
     }
 
     @ApiOperation(value = "审核机构信息接口")
     @GetMapping("/auditInstitution")
     public BaseResponse auditInstitution(@RequestParam @ApiParam String institutionId, @RequestParam @ApiParam Boolean enabled, @RequestParam(required = false) @ApiParam String remark) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.UPDATE, JSON.toJSONString(this.getRequest().getParameterMap()),
+                "审核机构信息接口"));
         return institutionFeign.auditInstitution(institutionId, enabled, remark);
     }
 
--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/controller/MerchantFeignController.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/controller/MerchantFeignController.java
@@ -1,10 +1,13 @@
 package com.asianwallets.permissions.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.asianwallets.common.base.BaseController;
+import com.asianwallets.common.constant.AsianWalletConstant;
 import com.asianwallets.common.dto.MerchantDTO;
 import com.asianwallets.common.response.BaseResponse;
 import com.asianwallets.common.response.ResultUtil;
 import com.asianwallets.permissions.feign.base.MerchantFeign;
+import com.asianwallets.permissions.service.OperationLogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -24,45 +27,69 @@ public class MerchantFeignController extends BaseController {
     @Autowired
     private MerchantFeign merchantFeign;
 
+    @Autowired
+    private OperationLogService operationLogService;
+
     @ApiOperation(value = "添加商户")
     @PostMapping("addMerchant")
     public BaseResponse addMerchant(@RequestBody @ApiParam MerchantDTO merchantDTO) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.ADD, JSON.toJSONString(merchantDTO),
+                "添加商户"));
         return merchantFeign.addMerchant(merchantDTO);
     }
 
     @ApiOperation(value = "修改商户")
     @PostMapping("updateMerchant")
     public BaseResponse updateMerchant(@RequestBody @ApiParam MerchantDTO merchantDTO) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.UPDATE, JSON.toJSONString(merchantDTO),
+                "修改商户"));
         return merchantFeign.updateMerchant(merchantDTO);
     }
 
     @ApiOperation(value = "分页查询商户信息列表")
     @PostMapping("/pageFindMerchant")
     public BaseResponse pageFindMerchant(@RequestBody @ApiParam MerchantDTO merchantDTO) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(merchantDTO),
+                "分页查询商户信息列表"));
         return merchantFeign.pageFindMerchant(merchantDTO);
     }
 
     @ApiOperation(value = "分页查询商户审核信息列表")
     @PostMapping("/pageFindMerchantAudit")
     public BaseResponse pageFindMerchantAudit(@RequestBody @ApiParam MerchantDTO merchantDTO) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(merchantDTO),
+                "分页查询商户审核信息列表"));
         return merchantFeign.pageFindMerchantAudit(merchantDTO);
     }
 
     @ApiOperation(value = "根据商户Id查询商户信息详情")
     @GetMapping("/getMerchantInfo")
     public BaseResponse getMerchantInfo(@RequestParam @ApiParam String id) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(id),
+                "根据商户Id查询商户信息详情"));
         return merchantFeign.getMerchantInfo(id);
     }
 
     @ApiOperation(value = "根据商户Id查询商户审核信息详情")
     @GetMapping("/getMerchantAuditInfo")
     public BaseResponse getMerchantAuditInfo(@RequestParam @ApiParam String id) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(id),
+                "根据商户Id查询商户审核信息详情"));
         return merchantFeign.getMerchantAuditInfo(id);
     }
 
     @ApiOperation(value = "审核商户信息接口")
     @GetMapping("/auditMerchant")
     public BaseResponse auditMerchant(@RequestParam @ApiParam String merchantId, @RequestParam @ApiParam Boolean enabled, @RequestParam(required = false) @ApiParam String remark) {
+        //添加操作日志
+        operationLogService.addOperationLog(this.setOperationLog(this.getSysUserVO().getUsername(), AsianWalletConstant.UPDATE, JSON.toJSONString(this.getRequest().getParameterMap()),
+                "审核商户信息接口"));
         return merchantFeign.auditMerchant(merchantId, enabled, remark);
     }
 }
