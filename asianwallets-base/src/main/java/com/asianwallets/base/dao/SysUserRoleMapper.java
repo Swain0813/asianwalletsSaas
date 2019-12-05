8b5a88e8ecfe65f8ade892c5245a628a9b1f1951
DESKTOP-311TI2U\yangs <yangshanlong@asianwallets.com>
2019-12-05 10:56:46 +0800

    追加角色的启用禁用的查询条件

--- asianwallets-base/src/main/java/com/asianwallets/base/dao/SysUserRoleMapper.java
+++ asianwallets-base/src/main/java/com/asianwallets/base/dao/SysUserRoleMapper.java
@@ -1,5 +1,4 @@
 package com.asianwallets.base.dao;
-
 import com.asianwallets.common.base.BaseMapper;
 import com.asianwallets.common.entity.SysUserRole;
 import org.apache.ibatis.annotations.Select;
@@ -8,18 +7,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SysUserRoleMapper extends BaseMapper<SysUserRole> {
 
-    @Select("select id from sys_role where role_name like '机构管理员%'")
+    @Select("select id from sys_role where role_name like '机构管理员%' and enabled=1")
     String getInstitutionRoleId();
 
-    @Select("select id from sys_role where role_name like 'POS机管理员%'")
+    @Select("select id from sys_role where role_name like 'POS机管理员%' and enabled=1")
     String getPOSRoleId();
 
-    @Select("select id from sys_role where role_name like '代理管理员%'")
+    @Select("select id from sys_role where role_name like '代理管理员%' and enabled=1")
     String getAgencyRoleId();
 
-    @Select("select id from sys_role where role_name like '商户管理员%'")
+    @Select("select id from sys_role where role_name like '商户管理员%' and enabled=1")
     String getMerchantRoleId();
 
-    @Select("select id from sys_role where role_name like '集团商户管理员%'")
+    @Select("select id from sys_role where role_name like '集团商户管理员%' and enabled=1")
     String getGroupRoleId();
 }
