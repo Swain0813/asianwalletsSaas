9f8b56bf3f709a2c80c713c5107249ddbf2c1f89
XuWenQi <xuwq@asianwallets.com>
2019-12-05 15:06:48 +0800

    1

---
+++ asianwallets-base/src/main/java/com/asianwallets/base/dao/InstitutionProductChannelMapper.java
@@ -0,0 +1,10 @@
+package com.asianwallets.base.dao;
+
+import com.asianwallets.common.base.BaseMapper;
+import com.asianwallets.common.entity.InstitutionProductChannel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstitutionProductChannelMapper extends BaseMapper<InstitutionProductChannel> {
+
+}
