4b3eec9d6437d11e5a4a7da8b8bccca686b5163a
XuWenQi <xuwq@asianwallets.com>
2019-12-06 13:45:53 +0800

    机构产品通道

--- asianwallets-base/src/main/java/com/asianwallets/base/dao/InstitutionChannelMapper.java
+++ asianwallets-base/src/main/java/com/asianwallets/base/dao/InstitutionChannelMapper.java
@@ -4,7 +4,7 @@ import com.asianwallets.common.base.BaseMapper;
 import com.asianwallets.common.entity.InstitutionChannel;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
+import java.util.List;
 
 @Repository
 public interface InstitutionChannelMapper extends BaseMapper<InstitutionChannel> {
@@ -12,8 +12,8 @@ public interface InstitutionChannelMapper extends BaseMapper<InstitutionChannel>
     /**
      * 根据机构产品ID删除机构通道信息
      *
-     * @param insProIdSet 机构产品ID集合
+     * @param insProIdList 机构产品ID集合
      * @return 修改条数
      */
-    int deleteByInsProIdList(Set<String> insProIdSet);
+    int deleteByInsProIdList(List<String> insProIdList);
 }
