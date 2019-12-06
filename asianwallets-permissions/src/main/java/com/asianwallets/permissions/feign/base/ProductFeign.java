fa57f50b9c977af46e8dcbcebef0c0e4e9957849
yangxu <yangxu@asianwallets.com>
2019-12-06 11:03:32 +0800

    1

--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/ProductFeign.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/ProductFeign.java
@@ -3,11 +3,9 @@ package com.asianwallets.permissions.feign.base;
 
 import com.asianwallets.common.dto.ProductDTO;
 import com.asianwallets.common.response.BaseResponse;
-import com.asianwallets.common.response.ResultUtil;
 import com.asianwallets.permissions.feign.base.impl.ProductFeignImpl;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
