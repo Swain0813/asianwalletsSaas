12be6ed3402ad08220cccad030be93ac2ffe04d4
Yanlaid <shenxinr@qq.com>
2019-12-09 16:13:03 +0800

    --

--- asianwallets-base/src/main/java/com/asianwallets/base/controller/MccChannelController.java
+++ asianwallets-base/src/main/java/com/asianwallets/base/controller/MccChannelController.java
@@ -41,6 +41,7 @@ public class MccChannelController extends BaseController {
     @ApiOperation(value = "查询mccChannel")
     @PostMapping("pageMccChannel")
     public BaseResponse pageMccChannel(@RequestBody @ApiParam MccChannelDTO mc) {
+        mc.setLanguage(this.getLanguage());
         return ResultUtil.success(mccChannelService.pageMccChannel(mc));
     }
 
