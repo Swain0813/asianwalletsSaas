860b3ff91a142691ae5f39e44201acf47ff87348
XuWenQi <xuwq@asianwallets.com>
2019-12-10 16:10:45 +0800

    测试银行

--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/controller/BankFeignController.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/controller/BankFeignController.java
@@ -1,27 +1,43 @@
 package com.asianwallets.permissions.controller;
 
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.alibaba.fastjson.JSON;
 import com.asianwallets.common.base.BaseController;
+import com.asianwallets.common.cache.CommonLanguageCacheService;
 import com.asianwallets.common.constant.AsianWalletConstant;
 import com.asianwallets.common.dto.BankDTO;
+import com.asianwallets.common.exception.BusinessException;
 import com.asianwallets.common.response.BaseResponse;
+import com.asianwallets.common.response.EResultEnum;
+import com.asianwallets.common.response.ResultUtil;
+import com.asianwallets.common.utils.ArrayUtil;
+import com.asianwallets.common.utils.ExcelUtils;
+import com.asianwallets.common.utils.SpringContextUtil;
+import com.asianwallets.common.vo.ChannelExportVO;
 import com.asianwallets.common.vo.ExportBankVO;
 import com.asianwallets.permissions.feign.base.BankFeign;
 import com.asianwallets.permissions.service.OperationLogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
 @Api(description = "银行接口")
 @RequestMapping("/bank")
+@Slf4j
 public class BankFeignController extends BaseController {
 
     @Autowired
@@ -56,9 +72,28 @@ public class BankFeignController extends BaseController {
 
     @ApiOperation(value = "导出银行信息")
     @PostMapping("/exportBank")
-    public List<ExportBankVO> exportBank(@RequestBody @ApiParam BankDTO bankDTO) {
+    public BaseResponse exportBank(@RequestBody @ApiParam BankDTO bankDTO, HttpServletResponse response) {
         operationLogService.addOperationLog(setOperationLog(getSysUserVO().getUsername(), AsianWalletConstant.SELECT, JSON.toJSONString(bankDTO),
                 "导出银行信息"));
-        return bankFeign.exportBank(bankDTO);
+        ExcelWriter writer = ExcelUtil.getBigWriter();
+        try {
+            List<ExportBankVO> dataList = bankFeign.exportBank(bankDTO);
+            ServletOutputStream out = response.getOutputStream();
+            if (ArrayUtil.isEmpty(dataList)) {
+                HashMap errorMsgMap = SpringContextUtil.getBean(CommonLanguageCacheService.class).getLanguage(getLanguage());
+                writer.write(Arrays.asList("message", errorMsgMap.get(String.valueOf(EResultEnum.DATA_IS_NOT_EXIST.getCode()))));
+                writer.flush(out);
+                return ResultUtil.success();
+            }
+            ExcelUtils excelUtils = new ExcelUtils();
+            excelUtils.exportExcel(dataList, ExportBankVO.class, writer);
+            writer.flush(out);
+        } catch (Exception e) {
+            log.info("==========【导出银行信息】==========【导出银行信息异常】", e);
+            throw new BusinessException(EResultEnum.INSTITUTION_INFORMATION_EXPORT_FAILED.getCode());
+        } finally {
+            writer.close();
+        }
+        return ResultUtil.success();
     }
 }
