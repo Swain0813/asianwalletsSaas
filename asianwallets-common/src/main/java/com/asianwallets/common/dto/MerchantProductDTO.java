e98ab428a33677ff49883d9b39d6be341ee8a1eb
yangxu <yangxu@asianwallets.com>
2019-12-11 14:16:24 +0800

    1

--- asianwallets-common/src/main/java/com/asianwallets/common/dto/MerchantProductDTO.java
+++ asianwallets-common/src/main/java/com/asianwallets/common/dto/MerchantProductDTO.java
@@ -6,7 +6,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.persistence.Column;
 import java.math.BigDecimal;
 import java.util.Date;
 
