c634bd8c040536f21c383f7b396896e6881f3799
DESKTOP-311TI2U\yangs <yangshanlong@asianwallets.com>
2019-12-12 15:56:02 +0800

    更新

--- asianwallets-channels/src/main/java/com/asianwallets/channels/dao/ChannelsOrderMapper.java
+++ asianwallets-channels/src/main/java/com/asianwallets/channels/dao/ChannelsOrderMapper.java
@@ -10,9 +10,21 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ChannelsOrderMapper extends BaseMapper<ChannelsOrder> {
 
+    /**
+     * 根据订单号以及交易状态更新付款中的上报通道订单记录
+     * @param orderId
+     * @param channelNumber
+     * @param status
+     * @return
+     */
     @Update("update channels_order set trade_status = #{status},channel_number = #{channelNumber},update_time= NOW() where id = #{orderId} and trade_status = 1")
     int updateStatusById(@Param("orderId") String orderId, @Param("channelNumber") String channelNumber, @Param("status") String status);
 
+    /**
+     * 根据订单号确认上报通道订单记录表是不是存在该记录
+     * @param orderId
+     * @return
+     */
     @Select("select count(1) from channels_order where id = #{orderId}")
     int selectCountById(@Param("orderId") String orderId);
 }
