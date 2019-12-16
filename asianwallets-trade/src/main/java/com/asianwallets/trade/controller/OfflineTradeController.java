97278dcdeb5f4072df7ede926b8b0a9cb16bd1f5
XuWenQi <xuwq@asianwallets.com>
2019-12-16 16:16:29 +0800

    11

--- asianwallets-trade/src/main/java/com/asianwallets/trade/controller/OfflineTradeController.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/controller/OfflineTradeController.java
@@ -2,7 +2,6 @@ package com.asianwallets.trade.controller;
 
 import com.asianwallets.common.base.BaseController;
 import com.asianwallets.common.response.BaseResponse;
-import com.asianwallets.common.response.EResultEnum;
 import com.asianwallets.common.response.ResultUtil;
 import com.asianwallets.trade.dto.CsbDynamicScanDTO;
 import com.asianwallets.trade.service.OfflineTradeService;
@@ -10,7 +9,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -26,18 +24,7 @@ public class OfflineTradeController extends BaseController {
     @ApiOperation(value = "线下同机构CSB动态扫码")
     @PostMapping("csbDynamicScan")
     public BaseResponse csbDynamicScan(@RequestBody @ApiParam @Valid CsbDynamicScanDTO csbDynamicScanDTO) {
-        BaseResponse baseResponse = offlineTradeService.csbDynamicScan(csbDynamicScanDTO);
-        String code = baseResponse.getCode();
-        if (StringUtils.isEmpty(code)) {
-            baseResponse.setCode(EResultEnum.SUCCESS.getCode());
-            baseResponse.setMsg("SUCCESS");
-            return baseResponse;
-        }
-        //code不为空 msg不为空 返回通道的错误信息
-        if (!StringUtils.isEmpty(baseResponse.getMsg())) {
-            return baseResponse;
-        }
-        return ResultUtil.error(code, getErrorMsgMap(code));
+        return ResultUtil.success(offlineTradeService.csbDynamicScan(csbDynamicScanDTO));
     }
 }
 
--- asianwallets-trade/src/main/java/com/asianwallets/trade/service/impl/OfflineTradeServiceImpl.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/service/impl/OfflineTradeServiceImpl.java
@@ -1,16 +1,26 @@
 package com.asianwallets.trade.service.impl;
 
+import com.asianwallets.common.entity.Orders;
 import com.asianwallets.common.exception.BusinessException;
 import com.asianwallets.common.response.BaseResponse;
+import com.asianwallets.common.response.EResultEnum;
+import com.asianwallets.common.utils.DateToolUtils;
+import com.asianwallets.trade.dao.OrdersMapper;
 import com.asianwallets.trade.dto.CsbDynamicScanDTO;
 import com.asianwallets.trade.service.OfflineTradeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 @Slf4j
 public class OfflineTradeServiceImpl implements OfflineTradeService {
+
+    @Autowired
+    private OrdersMapper ordersMapper;
 
     /**
      * 线下同机构CSB动态扫码
@@ -19,8 +29,22 @@ public class OfflineTradeServiceImpl implements OfflineTradeService {
      * @return BaseResponse
      */
     @Override
-    @Transactional(noRollbackFor = BusinessException.class)
+    @Transactional(rollbackFor = Exception.class, noRollbackFor = BusinessException.class)
     public BaseResponse csbDynamicScan(CsbDynamicScanDTO csbDynamicScanDTO) {
+        Orders orders = new Orders();
+        orders.setMerchantId(csbDynamicScanDTO.getMerchantId());
+        orders.setMerchantOrderId(csbDynamicScanDTO.getOrderNo());
+        orders.setOrderCurrency(csbDynamicScanDTO.getOrderCurrency());
+        orders.setOrderAmount(csbDynamicScanDTO.getOrderAmount());
+        orders.setMerchantOrderTime(DateToolUtils.getReqDateG(csbDynamicScanDTO.getOrderTime()));
+        orders.setProductCode(csbDynamicScanDTO.getProductCode());
+        orders.setImei(csbDynamicScanDTO.getImei());
+        orders.setOperatorId(csbDynamicScanDTO.getOperatorId());
+        orders.setCreateTime(new Date());
+        orders.setCreator(csbDynamicScanDTO.getMerchantId());
+        ordersMapper.insert(orders);
+//        throw new BusinessException(EResultEnum.DATA_IS_NOT_EXIST.getCode());
+//        int i = 1 / 0;
         return null;
     }
 }
