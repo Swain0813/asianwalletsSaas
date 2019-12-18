4b6196b423a44e7acb72f18ded817cc503a4bc32
XuWenQi <xuwq@asianwallets.com>
2019-12-18 11:53:46 +0800

    11

--- asianwallets-trade/src/main/java/com/asianwallets/trade/feign/Impl/MessageFeignImpl.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/feign/Impl/MessageFeignImpl.java
@@ -1,4 +1,4 @@
-package com.asianwallets.trade.feign.Impl;
+package com.asianwallets.trade.feign.impl;
 import com.asianwallets.common.enums.Status;
 import com.asianwallets.common.exception.BusinessException;
 import com.asianwallets.common.response.BaseResponse;
@@ -8,43 +8,19 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-/**
- *短信和邮件相关业务
- */
 @Component
 public class MessageFeignImpl implements MessageFeign {
 
-    /**
-     * 发送简单邮件
-     * @param sendTo
-     * @param title
-     * @param content
-     * @return
-     */
     @Override
     public BaseResponse sendSimpleMail(String sendTo, String title, String content) {
         throw new BusinessException(EResultEnum.ERROR.getCode());
     }
 
-    /**
-     * 国内普通发送
-     * @param mobile
-     * @param content
-     * @return
-     */
     @Override
     public BaseResponse sendSimple(String mobile, String content) {
         throw new BusinessException(EResultEnum.ERROR.getCode());
     }
 
-    /**
-     * 发送模板邮件
-     * @param sendTo
-     * @param languageNum
-     * @param templateNum
-     * @param param
-     * @return
-     */
     @Override
     public BaseResponse sendTemplateMail(String sendTo, String languageNum, Status templateNum, Map<String, Object> param) {
         throw new BusinessException(EResultEnum.ERROR.getCode());
