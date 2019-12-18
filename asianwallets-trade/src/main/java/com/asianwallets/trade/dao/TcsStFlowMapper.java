05988bcf8ba5fecbb0b56fdf15e1722c65d2d626
yangxu <yangxu@asianwallets.com>
2019-12-18 17:42:42 +0800

    1

---
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/dao/TcsStFlowMapper.java
@@ -0,0 +1,34 @@
+package com.asianwallets.trade.dao;
+
+import com.asianwallets.common.base.BaseMapper;
+import com.asianwallets.common.entity.TcsStFlow;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Repository
+public interface TcsStFlowMapper extends BaseMapper<TcsStFlow> {
+
+
+    /**
+     * @Author YangXu
+     * @Date 2019/7/29
+     * @Descripate 查询所有date时间之前的未结算订单
+     * @return
+     **/
+    List<TcsStFlow> selectList(TcsStFlow tcsStFlow);
+
+
+    /**
+     * @Author YangXu
+     * @Date 2019/12/12
+     * @Descripate 查询未结算的金额
+     * @return
+     **/
+    @Select("select sum(txnamount-fee+refundOrderFee) as unSettleAmount from tcs_stflow where merchantid = #{merchantid} and txncurrency = #{txncurrency} and STstate=1")
+    BigDecimal getUnSettleAmount(@Param("merchantid") String merchantid, @Param("txncurrency") String txncurrency);
+}
