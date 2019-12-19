b4065c8a55ef960925f677b7a8d2044e1ebc2052
yangxu <yangxu@asianwallets.com>
2019-12-19 11:31:43 +0800

    1

---
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/dao/AccountMapper.java
@@ -0,0 +1,46 @@
+package com.asianwallets.trade.dao;
+
+import com.asianwallets.common.base.BaseMapper;
+import com.asianwallets.common.entity.Account;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+  * 账户表 Mapper 接口
+ * </p>
+ *
+ * @author yx
+ * @since 2019-11-25
+ */
+@Repository
+public interface AccountMapper extends  BaseMapper<Account> {
+
+    /**
+     * 根据机构code和币种查询账户的结算金额和冻结金额
+     * @param merchantId
+     * @param currency
+     * @return
+     */
+    @Select("select id,settle_balance as settleBalance,freeze_balance as freezeBalance,clear_balance as clearBalance from account  where merchant_id = #{merchantId} and currency = #{currency} and enabled=true")
+    Account getAccount(@Param("merchantId") String merchantId, @Param("currency") String currency);
+
+    /**
+     * 根据机构code和币种获取账户信息
+     * @param institutionId
+     * @param currency
+     * @return
+     */
+    @Select("select account_code from account where institution_id = #{institutionId} and settle_currency = #{currency}")
+    String getAccountCode(@Param("institutionId") String institutionId, @Param("currency") String currency);
+
+    /**
+     * @Author YangXu
+     * @Date 2019/1/29
+     * @Descripate 根据币种和机构id查询数量
+     * @return
+     **/
+    @Select("select count(1) from account where merchant_id = #{merchantId} and currency = #{currency}")
+    int getCountByinstitutionIdAndCurry(@Param("merchantId") String merchantId, @Param("currency") String currency);
+}
