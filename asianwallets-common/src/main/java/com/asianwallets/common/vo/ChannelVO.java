d002d348d7fdb5d5579a446b4d775869f2dee786
DESKTOP-311TI2U\yangs <yangshanlong@asianwallets.com>
2019-12-19 12:00:59 +0800

    更新

--- asianwallets-common/src/main/java/com/asianwallets/common/vo/ChannelVO.java
+++ asianwallets-common/src/main/java/com/asianwallets/common/vo/ChannelVO.java
@@ -1,10 +1,9 @@
 package com.asianwallets.common.vo;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
@@ -27,12 +26,13 @@ public class ChannelVO {
     @ApiModelProperty(value = "通道币种")
     private String currency;
 
-
     @ApiModelProperty(value = "通道最小限额")
-    private String limitMinAmount;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal limitMinAmount;
 
     @ApiModelProperty(value = "通道最大限额")
-    private String limitMaxAmount;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal limitMaxAmount;
 
     @ApiModelProperty(value = "通道状态")
     private Boolean enabled;
