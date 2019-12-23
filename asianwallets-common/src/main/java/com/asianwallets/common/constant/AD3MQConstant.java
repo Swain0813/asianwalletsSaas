e9640f15eb88926806f5f7b7e2eefcee049a0677
yangxu <yangxu@asianwallets.com>
2019-12-23 11:17:46 +0800

    1

--- asianwallets-common/src/main/java/com/asianwallets/common/constant/AD3MQConstant.java
+++ asianwallets-common/src/main/java/com/asianwallets/common/constant/AD3MQConstant.java
@@ -9,10 +9,10 @@ public class AD3MQConstant {
 
     /********************************************************  退款接口相关队列 **********************************************************************/
     //退款RF请求失败
-    public final static String TK_RF_FAIL_DL = AD3MQConstant.TK_RF_FAIL_DL;
+    public final static String TK_RF_FAIL_DL = "TK_RF_FAIL_DL";
     //调账失败队列
-    public final static String RA_AA_FAIL_DL = AD3MQConstant.RA_AA_FAIL_DL;
+    public final static String RA_AA_FAIL_DL = "RA_AA_FAIL_DL";
 
-    public final static String TK_SB_FAIL_DL = AD3MQConstant.TK_SB_FAIL_DL;
+    public final static String TK_SB_FAIL_DL = "TK_SB_FAIL_DL";
 
 }
