8afb6566307fcfc605c6715af0e5d3a19b1e5381
yangshanlong <yangshanlong@asianwallets.com>
2020-01-02 15:55:21 +0800

    11

--- asianwallets-trade/src/main/java/com/asianwallets/trade/vo/PosMerProVO.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/vo/PosMerProVO.java
@@ -17,11 +17,11 @@ public class PosMerProVO {
     @ApiModelProperty(value = "支付方式名称")
     private String payTypeName;
 
-    @ApiModelProperty(value = "产品支付方式logo1")
-    private String picon;
+    @ApiModelProperty(value = "产品详情图片")
+    private String productDetailsLogo;
 
-    @ApiModelProperty(value = "产品支付方式logo2")
-    private String dicon;
+    @ApiModelProperty(value = "产品打印图片")
+    private String productPrintLogo;
 
     @ApiModelProperty(value = "支付方式标记")
     private String flag;
