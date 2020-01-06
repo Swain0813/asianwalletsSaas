c2acd08b005bcf7f1bbf2eb4f2e108a2ab975e47
yangxu <yangxu@asianwallets.com>
2020-01-06 14:04:33 +0800

    1

---
+++ asianwallets-clearing/src/main/java/com/asianwallets/clearing/service/impl/CalculateShareBenefitServiceImpl.java
@@ -0,0 +1,9 @@
+package com.asianwallets.clearing.service.impl;
+
+/**
+ * @description:
+ * @author: YangXu
+ * @create: 2020-01-06 13:50
+ **/
+public class CalculateShareBenefitServiceImpl {
+}
---
+++ asianwallets-clearing/src/main/java/com/asianwallets/clearing/service/impl/ShareBenefitServiceImpl.java
@@ -0,0 +1,9 @@
+package com.asianwallets.clearing.service.impl;
+
+/**
+ * @description:
+ * @author: YangXu
+ * @create: 2020-01-06 11:40
+ **/
+public class ShareBenefitServiceImpl {
+}
