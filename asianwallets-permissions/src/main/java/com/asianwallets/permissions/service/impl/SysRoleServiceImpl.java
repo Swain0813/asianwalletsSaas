fb92aac45a72924f219da1bd5c6a64daddd67aab
XuWenQi <xuwq@asianwallets.com>
2020-03-30 13:55:06 +0800

    11

--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/service/impl/SysRoleServiceImpl.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/service/impl/SysRoleServiceImpl.java
@@ -147,7 +147,7 @@ public class SysRoleServiceImpl implements SysRoleService {
                 }
             }
         }
-        return new PageInfo<>(sysRoleMapper.pageGetSysRole(sysRoleDto));
+        return new PageInfo<>(sysRoles);
     }
 
 }
