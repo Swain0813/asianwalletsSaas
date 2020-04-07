1e7f875b649607f1db09161c030a864c1006304b
XinRanShen <shenxinr@qq.com>
2020-04-07 16:18:48 +0800

    style(修复订单导出头部多余一格):

--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/service/impl/ExportServiceImpl.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/service/impl/ExportServiceImpl.java
@@ -222,7 +222,8 @@ public class ExportServiceImpl implements ExportService {
             totalFee = totalFee.add(order.getFee());
             totalChannelFee = totalChannelFee.add(order.getChannelFee());
         }
-        writer.setColumnWidth(-1, 39);
+        //设置列宽
+        writer.setColumnWidth(-1, 40);
 //        StyleSet style = writer.getStyleSet();
 //        CellStyle cellStyle = style.getHeadCellStyle();
 //        cellStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.BLUE.getIndex());
@@ -331,6 +332,8 @@ public class ExportServiceImpl implements ExportService {
                     break;
             }
         }
+        //为了移除表头多余一格
+        statistics.remove(statistics.size() - 1);
         oList1.add(0, statistics);
         oList1.add(1, oSet1);
         writer.write(oList1);
