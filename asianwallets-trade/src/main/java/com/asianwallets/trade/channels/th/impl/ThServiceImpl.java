12eed1e9c3e0211ed953a961817db4693f946b40
XuWenQi <xuwq@asianwallets.com>
2020-05-09 14:20:09 +0800

    11

--- asianwallets-trade/src/main/java/com/asianwallets/trade/channels/th/impl/ThServiceImpl.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/channels/th/impl/ThServiceImpl.java
@@ -1,16 +1,20 @@
 package com.asianwallets.trade.channels.th.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.asianwallets.common.constant.AD3Constant;
 import com.asianwallets.common.constant.TradeConstant;
 import com.asianwallets.common.dto.th.ISO8583.ISO8583DTO;
 import com.asianwallets.common.entity.Channel;
+import com.asianwallets.common.entity.ChannelsOrder;
 import com.asianwallets.common.entity.Currency;
 import com.asianwallets.common.entity.Orders;
 import com.asianwallets.common.exception.BusinessException;
 import com.asianwallets.common.response.BaseResponse;
 import com.asianwallets.common.response.EResultEnum;
+import com.asianwallets.common.utils.DateToolUtils;
 import com.asianwallets.trade.channels.ChannelsAbstractAdapter;
 import com.asianwallets.trade.channels.th.ThService;
+import com.asianwallets.trade.dao.ChannelsOrderMapper;
 import com.asianwallets.trade.feign.ChannelsFeign;
 import com.asianwallets.trade.utils.HandlerType;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Slf4j
 @Service
@@ -26,6 +31,9 @@ public class ThServiceImpl extends ChannelsAbstractAdapter implements ThService 
 
     @Autowired
     private ChannelsFeign channelsFeign;
+
+    @Autowired
+    private ChannelsOrderMapper channelsOrderMapper;
 
     /**
      * 通华主扫接口
@@ -36,12 +44,30 @@ public class ThServiceImpl extends ChannelsAbstractAdapter implements ThService 
      */
     @Override
     public BaseResponse offlineCSB(Orders orders, Channel channel) {
+        ChannelsOrder channelsOrder = new ChannelsOrder();
+        channelsOrder.setId(orders.getId());
+        channelsOrder.setMerchantOrderId(orders.getMerchantOrderId());
+        channelsOrder.setTradeCurrency(orders.getTradeCurrency());
+        channelsOrder.setTradeAmount(orders.getTradeAmount());
+        channelsOrder.setReqIp(orders.getReqIp());
+        channelsOrder.setServerUrl(orders.getServerUrl());
+        channelsOrder.setTradeStatus(TradeConstant.TRADE_WAIT);
+        channelsOrder.setIssuerId(orders.getIssuerId());
+        channelsOrder.setOrderType(AD3Constant.TRADE_ORDER);
+        channelsOrder.setMd5KeyStr(channel.getMd5KeyStr());
+        channelsOrder.setPayerPhone(orders.getPayerPhone());
+        channelsOrder.setPayerName(orders.getPayerName());
+        channelsOrder.setPayerBank(orders.getPayerBank());
+        channelsOrder.setPayerEmail(orders.getPayerEmail());
+        channelsOrder.setCreateTime(new Date());
+        channelsOrder.setCreator(orders.getCreator());
+        channelsOrderMapper.insert(channelsOrder);
+
         ISO8583DTO iso8583DTO = new ISO8583DTO();
         //交易处理码
         iso8583DTO.setProcessingCode_3("000000");
         String tradeAmountStr = String.valueOf(orders.getTradeAmount());
         int tradeAmount = 0;
-        //交易金额
         if (new BigDecimal(orders.getTradeAmount().intValue()).compareTo(orders.getTradeAmount()) == 0) {
             //整数
             tradeAmount = orders.getTradeAmount().intValue();
@@ -53,24 +79,36 @@ public class ThServiceImpl extends ChannelsAbstractAdapter implements ThService 
         }
         //12位,左边填充0
         String formatAmount = String.format("%012d", tradeAmount);
+        //交易金额
         iso8583DTO.setAmountOfTransactions_4(formatAmount);
-        //iso8583DTO.setAmountOfTips_5("");
+        //iso8583DTO.setAmountOfTips_5(""); TODO
+        //受卡方系统跟踪号 TODO
         iso8583DTO.setSystemTraceAuditNumber_11("");
-        iso8583DTO.setTimeOfLocalTransaction_12("");
-        iso8583DTO.setDateOfLocalTransaction_13("");
-        iso8583DTO.setPointOfServiceEntryMode_22("");
-        iso8583DTO.setPointOfServiceConditionMode_25("");
+        //受卡方所在地时间HHmmss
+        iso8583DTO.setTimeOfLocalTransaction_12(DateToolUtils.getReqTimeHHmmss());
+        //受卡方所在地日期MMdd
+        iso8583DTO.setDateOfLocalTransaction_13(DateToolUtils.getReqTimeMMdd());
+        //服务点输入方式码 TODO
+        iso8583DTO.setPointOfServiceEntryMode_22("021");
+        //服务点条件码 TODO
+        iso8583DTO.setPointOfServiceConditionMode_25("00");
+        //受理方标识码 TODO
         iso8583DTO.setAcquiringInstitutionIdentificationCode_32("");
-        iso8583DTO.setRetrievalReferenceNumber_37("");
-        iso8583DTO.setResponseCode_39("");
+        //iso8583DTO.setRetrievalReferenceNumber_37("");
+        //iso8583DTO.setResponseCode_39("");
+        //受卡机终端标识码 TODO
         iso8583DTO.setCardAcceptorTerminalIdentification_41("");
+        //受卡方标识码 (受卡方的标识码，即商户代码)
         iso8583DTO.setCardAcceptorIdentificationCode_42("");
         //iso8583DTO.setAdditionalData_46("");
         //iso8583DTO.setAdditionalDataPrivate_47("");
         //交易货币代码
         iso8583DTO.setCurrencyCodeOfTransaction_49("156");
+        //自定义域
         iso8583DTO.setReservedPrivate_60("");
+        //自定义域
         iso8583DTO.setReservedPrivate_63("");
+        //报文鉴别码
         iso8583DTO.setMessageAuthenticationCode_64("");
         log.info("==================【通华线下CSB】==================【调用Channels服务】【请求参数】 iso8583DTO: {}", JSON.toJSONString(iso8583DTO));
         BaseResponse channelResponse = channelsFeign.thCSB(iso8583DTO);
