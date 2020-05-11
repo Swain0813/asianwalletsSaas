173e86aa4cb8d3a6275199744eeb930940cb0ba4
yangxu <yangxu@asianwallets.com>
2020-05-11 16:36:22 +0800

    1

--- asianwallets-common/src/main/java/com/asianwallets/common/dto/th/ISO8583/demo.java
+++ asianwallets-common/src/main/java/com/asianwallets/common/dto/th/ISO8583/demo.java
@@ -13,7 +13,7 @@ public class demo {
         ISO8583DTO iso8583DTO = new ISO8583DTO();
         iso8583DTO.setMessageType("0800");
         iso8583DTO.setSystemTraceAuditNumber_11("198124");
-        iso8583DTO.setAcquiringInstitutionIdentificationCode_32("08600005");
+        iso8583DTO.setAcquiringInstitutionIdentificationCode_32("00008600005");
         iso8583DTO.setCardAcceptorTerminalIdentification_41("00018644");
         iso8583DTO.setCardAcceptorIdentificationCode_42("852999958120501");
         iso8583DTO.setReservedPrivate_60("50000001003");
@@ -27,9 +27,9 @@ public class demo {
             String strHex2 = String.format("%04x",sendMsg.length()/2);
             sendMsg = strHex2 + sendMsg;
             System.out.println(" ===  sendMsg  ====   "+sendMsg);
-            //String result = ISO8583Util.send8583(sendMsg,"58.248.241.169",10089);
+            String result = ISO8583Util.send8583(sendMsg,"58.248.241.169",10089);
 
-            //System.out.println(" ====  result  ===   "+result);
+            System.out.println(" ====  result  ===   "+result);
             // 解包
             ISO8583DTO iso8583DTO1281 = ISO8583Util.unpackISO8583DTO(sendMsg);
             System.out.println(iso8583DTO1281.toString());
