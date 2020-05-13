3b3f293e6d7aceb6e04e2ca977212ce1160b36e6
yangxu <yangxu@asianwallets.com>
2020-05-13 17:55:50 +0800

    1

--- asianwallets-common/src/main/java/com/asianwallets/common/dto/th/ISO8583/ISO8583Util.java
+++ asianwallets-common/src/main/java/com/asianwallets/common/dto/th/ISO8583/ISO8583Util.java
@@ -30,8 +30,9 @@ public class ISO8583Util {
 
     /**
      * 128域组包
-     * @param iso8583DTO128   报文交互DTO，128域
-     *                        4位报文长度 + 4位消息类型 + 32位BITMAP + 报文信息
+     *
+     * @param iso8583DTO128 报文交互DTO，128域
+     *                      4位报文长度 + 4位消息类型 + 32位BITMAP + 报文信息
      * @return
      */
     public static String packISO8583DTO(ISO8583DTO iso8583DTO128) throws IncorrectLengthException {
@@ -39,42 +40,44 @@ public class ISO8583Util {
         // 先拼接消息类型
         sendMsg.append(iso8583DTO128.getMessageType());
         // 拼接BITMAP + 报文信息
-        sendMsg.append(getBitMapAndMsg(iso8583DTO128, 64));
+        Object[] o = getBitMapAndMsg(iso8583DTO128, 64);
+        sendMsg.append(o[0]);
         // 计算报文长度，长度占4个字节，不足4字节左补0
-        int sendMsgLen = sendMsg.length();
+        int sendMsgLen = (int) o[1];
         String sendMsgLenStr = Integer.toString(sendMsgLen);
         sendMsgLenStr = NumberStringUtil.addLeftChar(sendMsgLenStr, 8, '0');
         sendMsgLenStr = NumberStringUtil.str2HexStr(sendMsgLenStr);
-        System.out.println("报文长度 = "+ sendMsgLenStr);
+        System.out.println("报文长度 = " + sendMsgLenStr);
         // 将4位报文长度插到最前边
-        sendMsg.insert(0,sendMsgLenStr);
+        sendMsg.insert(0, sendMsgLenStr);
 
         return sendMsg.toString();
     }
 
     /**
      * 128域解包
+     *
      * @param receivedMsg 收到的报文消息
      *                    4位报文长度 + 4位消息类型 + 32位BITMAP + 报文信息
      * @return
      * @throws IncorrectMessageException
      */
-    public static ISO8583DTO unpackISO8583DTO(String receivedMsg) throws IncorrectMessageException{
+    public static ISO8583DTO unpackISO8583DTO(String receivedMsg) throws IncorrectMessageException {
 
-        if(null == receivedMsg){
+        if (null == receivedMsg) {
             throw new IncorrectMessageException("报文为空");
         }
         int totalLen = receivedMsg.length();
-        if(totalLen < 40){
+        if (totalLen < 40) {
             throw new IncorrectMessageException("报文格式不正确，报文长度最少为40");
         }
 
-        int msgLen = Integer.valueOf(NumberStringUtil.hexStr2Str(receivedMsg.substring(148,164)));
+        int msgLen = Integer.valueOf(NumberStringUtil.hexStr2Str(receivedMsg.substring(148, 164)));
         //if(msgLen != totalLen - 4){
         //    throw new IncorrectMessageException("报文长度不匹配");
         //}
-        String messageType = receivedMsg.substring(164,168);
-        String hexBitMap = receivedMsg.substring(168,184);
+        String messageType = receivedMsg.substring(164, 168);
+        String hexBitMap = receivedMsg.substring(168, 184);
         String binaryBitMap = NumberStringUtil.hexToBinaryString(hexBitMap);
         //String binaryBitMap = hexBitMap;
         String[] binaryBitMapArgs = binaryBitMap.split("");
@@ -86,7 +89,7 @@ public class ISO8583Util {
         return iso8583DTO128;
     }
 
-    private static String getBitMapAndMsg(Object iso8583DTO, int bitLen) throws IncorrectLengthException {
+    private static Object[] getBitMapAndMsg(Object iso8583DTO, int bitLen) throws IncorrectLengthException {
         // 获取ISO8583DTO类的属性，key为fldIndex域序号，value为属性名
         Map<Integer, String> iso8583DTOFldMap = getISO8583DTOFldMap(iso8583DTO.getClass());
         // 初始化域位图
@@ -98,50 +101,54 @@ public class ISO8583Util {
         String fldValue;
         // 按照格式处理后的值
         String fldSendValue;
+        int len = 0;
         // 将每个域对应的值，保存到对应下标中
         String[] fldSendValues = new String[bitLen];
         try {
             // 循环判断哪个字段有值
-            for(Map.Entry<Integer, String> entry : iso8583DTOFldMap.entrySet()){
+            for (Map.Entry<Integer, String> entry : iso8583DTOFldMap.entrySet()) {
                 fldName = entry.getValue();
                 propertyDescriptor = new PropertyDescriptor(fldName, iso8583DTO.getClass());
                 fldValue = (String) propertyDescriptor.getReadMethod().invoke(iso8583DTO);
 
-                if(StringUtils.isNotEmpty(fldValue)){
+                if (StringUtils.isNotEmpty(fldValue)) {
                     // 如果此域有值，将对应的位图位置修改为1
-                    bitMap = bitMap.replace(entry.getKey()-1,entry.getKey(),"1");
+                    bitMap = bitMap.replace(entry.getKey() - 1, entry.getKey(), "1");
                     // 根据注解对值进行处理
                     Field field = iso8583DTO.getClass().getDeclaredField(fldName);
-                    fldSendValue = verifyAndTransValue(field,fldValue);
-                    fldSendValues[entry.getKey()-1] = fldSendValue;
+                    Object[] o = verifyAndTransValue(field, fldValue);
+                    fldSendValue = (String) o[0];
+                    len += (int) o[1];
+                    fldSendValues[entry.getKey() - 1] = fldSendValue;
                 }
             }
         } catch (IncorrectLengthException e) {
             throw e;
-        } catch (Exception e){
+        } catch (Exception e) {
             log.info("对象序列化报错");
-            return "";
+            return null;
         }
 
         // 将128位2进制位图转换为32位16进制数据
         String bitMapHexStr = NumberStringUtil.binaryToHexString(bitMap.toString());
         //String bitMapHexStr = bitMap.toString();
-        log.info("bitmap = "+ bitMapHexStr);
+        log.info("bitmap = " + bitMapHexStr);
         StringBuffer bitMapAndMsg = new StringBuffer();
         // 位图在前，先拼接位图
         bitMapAndMsg.append(bitMapHexStr);
         // 拼接报文数据
-        for(String value : fldSendValues){
-            if(StringUtils.isNotEmpty(value)){
+        for (String value : fldSendValues) {
+            if (StringUtils.isNotEmpty(value)) {
                 bitMapAndMsg.append(value);
             }
         }
 
-        return bitMapAndMsg.toString();
+        return new Object[]{bitMapAndMsg.toString(), len};
     }
 
     /**
      * 获取ISO8583DTO类的属性，key为fldIndex，value为属性名
+     *
      * @param clazz
      * @return
      */
@@ -160,7 +167,7 @@ public class ISO8583Util {
 
     private static Object msgToObject(Class clazz, String[] binaryBitMapArgs, String msg) {
 
-        try{
+        try {
             // 返回对象
             Object retObject = clazz.newInstance();
 
@@ -175,45 +182,45 @@ public class ISO8583Util {
             String fldValue;
             String type;
             // 循环位图，第一位表示64域或者128域，所以从第二位开始
-            for(int i=1,len=binaryBitMapArgs.length; i<len; i++){
-                if(Objects.equals(binaryBitMapArgs[i], "0")){
+            for (int i = 1, len = binaryBitMapArgs.length; i < len; i++) {
+                if (Objects.equals(binaryBitMapArgs[i], "0")) {
                     // 0表示没有值，跳过
                     continue;
                 }
 
                 // 位图下标从1开始，所以需要+1
-                fldName = iso8583DTOFldMap.get(i+1);
+                fldName = iso8583DTOFldMap.get(i + 1);
                 field = clazz.getDeclaredField(fldName);
                 fldAnnotation = field.getAnnotation(ISO8583Annotation.class);
                 fldFlag = fldAnnotation.fldFlag();
                 type = fldAnnotation.type();
-                if(Objects.equals(FldFlag.FIXED, fldFlag)){
+                if (Objects.equals(FldFlag.FIXED, fldFlag)) {
                     dataLength = fldAnnotation.dataFldLength();
-                }else if(Objects.equals(FldFlag.UNFIXED_2, fldFlag)){
-                    dataLength = Integer.valueOf(msg.substring(indexFlag, indexFlag=indexFlag+2));
-                }else if(Objects.equals(FldFlag.UNFIXED_3, fldFlag)){
-                    dataLength = Integer.valueOf(msg.substring(indexFlag, indexFlag=indexFlag+4));
-                }else{
+                } else if (Objects.equals(FldFlag.UNFIXED_2, fldFlag)) {
+                    dataLength = Integer.valueOf(msg.substring(indexFlag, indexFlag = indexFlag + 2));
+                } else if (Objects.equals(FldFlag.UNFIXED_3, fldFlag)) {
+                    dataLength = Integer.valueOf(msg.substring(indexFlag, indexFlag = indexFlag + 4));
+                } else {
                     //未知类型，不做处理
                     continue;
                 }
-                if(type.equals("ASC") || type.equals("BINARY")){
-                    dataLength = dataLength*2;
+                if (type.equals("ASC") || type.equals("BINARY")) {
+                    dataLength = dataLength * 2;
                 }
-                fldValue = msg.substring(indexFlag,indexFlag+dataLength);
-                if(dataLength%2 != 0){
-                    dataLength = dataLength+1;
+                fldValue = msg.substring(indexFlag, indexFlag + dataLength);
+                if (dataLength % 2 != 0) {
+                    dataLength = dataLength + 1;
                 }
                 indexFlag += dataLength;
                 field.setAccessible(true);
-                if(type.equals("ASC")){
+                if (type.equals("ASC")) {
                     fldValue = NumberStringUtil.hexStr2Str(fldValue);
                 }
-                field.set(retObject,fldValue);
+                field.set(retObject, fldValue);
             }
 
             return retObject;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.info("对象反序列化报错");
             return null;
         }
@@ -223,16 +230,17 @@ public class ISO8583Util {
 
     /**
      * 组包时根据字段原值按照其配置规则转为十六进制 PACK
+     *
      * @param field
-     * @param fldValue          字段值
-     * @exception Exception .
-     * */
-    private static String verifyAndTransValue(Field field, String fldValue)
+     * @param fldValue 字段值
+     * @throws Exception .
+     */
+    private static Object[] verifyAndTransValue(Field field, String fldValue)
             throws IncorrectLengthException {
-
+        Object[] o = new Object[]{};
         boolean fldHasAnnotation = field.isAnnotationPresent(ISO8583Annotation.class);
         if (!fldHasAnnotation) {
-            return fldValue;
+            return o;
         }
 
         ISO8583Annotation iso8583Annotation = field.getAnnotation(ISO8583Annotation.class);
@@ -242,46 +250,48 @@ public class ISO8583Util {
         int actualLen = fldValue.length();
 
         // 固定长度，则校验一下长度是否一致
-        if(Objects.equals(FldFlag.FIXED, fldFlag)){
-            if(actualLen != expectLen){
+        if (Objects.equals(FldFlag.FIXED, fldFlag)) {
+            if (actualLen != expectLen) {
                 String msg = String.format("%s长度不正确，期望长度为[%d]，实际长度为[%d]。"
-                        ,field.getName(),expectLen,actualLen);
+                        , field.getName(), expectLen, actualLen);
                 throw new IncorrectLengthException(msg);
             }
-            if(type.equals("ASC")){
-                fldValue=NumberStringUtil.str2HexStr(fldValue);
+            if (type.equals("ASC")) {
+                fldValue = NumberStringUtil.str2HexStr(fldValue);
             }
-            return fldValue;
+            return new Object[]{fldValue, actualLen};
         }
 
         // 可变长度，校验一下长度是否超过上限。如果长度符合，则在前边拼接长度值
-        if (Objects.equals(FldFlag.UNFIXED_2,fldFlag) || Objects.equals(FldFlag.UNFIXED_3,fldFlag)) {
-            if(actualLen > expectLen){
+        if (Objects.equals(FldFlag.UNFIXED_2, fldFlag) || Objects.equals(FldFlag.UNFIXED_3, fldFlag)) {
+            if (actualLen > expectLen) {
                 String msg = String.format("%s长度不正确，最大长度为[%d]，实际长度为[%d]。"
-                        ,field.getName(),expectLen,actualLen);
+                        , field.getName(), expectLen, actualLen);
                 throw new IncorrectLengthException(msg);
             }
 
             int len = 2;
-            if(Objects.equals(FldFlag.UNFIXED_3,fldFlag)){
+            if (Objects.equals(FldFlag.UNFIXED_3, fldFlag)) {
                 len = 3;
             }
             // 在报文前边拼接长度
-            if(type.equals("ASC")){
-                fldValue=NumberStringUtil.str2HexStr(fldValue);
+            if (type.equals("ASC")) {
+                fldValue = NumberStringUtil.str2HexStr(fldValue);
             }
-            fldValue = NumberStringUtil.addLeftChar(String.valueOf(actualLen),(len-1)*2, '0') + fldValue;
-            if(actualLen%2 != 0){
-                fldValue=NumberStringUtil.addRightChar(fldValue,1,'0');
+            fldValue = NumberStringUtil.addLeftChar(String.valueOf(actualLen), (len - 1) * 2, '0') + fldValue;
+            if (actualLen % 2 != 0) {
+                fldValue = NumberStringUtil.addRightChar(fldValue, 1, '0');
+                actualLen = actualLen + 1;
             }
-            return fldValue;
+            return new Object[]{fldValue, actualLen};
         }
-        return fldValue;
+        return o;
     }
 
     /**
      * 初始64和128域位图。
      * 64域的全是0；128域的除第一位为1，其它位全为0
+     *
      * @param d
      * @return
      */
@@ -289,18 +299,18 @@ public class ISO8583Util {
 
         StringBuffer bf = new StringBuffer();
 
-        if(d != 64 && d != 128){
+        if (d != 64 && d != 128) {
             return bf;
         }
 
-        if(d == 64){
+        if (d == 64) {
             bf.append("0");
-        }else{
+        } else {
             // 128域的第一位为"1"
             bf.append("1");
         }
 
-        for(int i = 1; i < d; i++){
+        for (int i = 1; i < d; i++) {
             bf.append("0");
         }
 
@@ -311,49 +321,47 @@ public class ISO8583Util {
      * 向服务器 发送8583报文
      *
      * @param send8583Str 发送给服务器的报文
-     *
-     * @param host 主机地址IP
-     *
-     * @param port 端口号
-     *
+     * @param host        主机地址IP
+     * @param port        端口号
      * @return 返回的数据
-     * */
-    public static String send8583(String send8583Str,String host,int port) throws Exception{
+     */
+    public static String send8583(String send8583Str, String host, int port) throws Exception {
         //客户端请求与本机在20011端口建立TCP连接
         Socket client = new Socket(host, port);
         client.setSoTimeout(70000);
         //获取Socket的输出流，用来发送数据到服务端
         PrintStream out = new PrintStream(client.getOutputStream());
         //获取Socket的输入流，用来接收从服务端发送过来的数据
-        InputStream buf =  client.getInputStream();
-        String str = "mpos-"+send8583Str;
+        InputStream buf = client.getInputStream();
+        String str = "mpos-" + send8583Str;
         //发送数据到服务端
         out.println(str);
-        try{
+        try {
             byte[] b = new byte[1024];
-            int rc=0;
+            int rc = 0;
             int c = 0;
-            while( (rc = buf.read(b, c, 1024) )>=0){
+            while ((rc = buf.read(b, c, 1024)) >= 0) {
                 c = buf.read(b, 0, rc);
             }
             String returnStr = byte2hex(b);
             String string = returnStr;
             String str16 = string.substring(0, 4);
-            int leng = Integer.parseInt(str16,16);
-            String result = string.substring(0, leng*2 + 4);
-            if (client!=null) {
+            int leng = Integer.parseInt(str16, 16);
+            String result = string.substring(0, leng * 2 + 4);
+            if (client != null) {
                 client.close();
             }
             return result;
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Time out, No response");
         }
-        if (client!=null) {
+        if (client != null) {
             client.close();
         }
         return null;
     }
+
     public static String byte2hex(byte[] b) // 二进制转字符串
     {
         String hs = "";
@@ -361,10 +369,9 @@ public class ISO8583Util {
         for (int n = 0; n < b.length; n++) {
             stmp = (java.lang.Integer.toHexString(b[n] & 0XFF));
 
-            if (stmp.length() == 1){
+            if (stmp.length() == 1) {
                 hs = hs + "0" + stmp;
-            }
-            else{
+            } else {
                 hs = hs + stmp;
             }
         }
--- asianwallets-common/src/main/java/com/asianwallets/common/dto/th/ISO8583/demo.java
+++ asianwallets-common/src/main/java/com/asianwallets/common/dto/th/ISO8583/demo.java
@@ -10,33 +10,33 @@ public class demo {
 
     public static void main(String[] args) {
 
-        //ISO8583DTO iso8583DTO = new ISO8583DTO();
-        //iso8583DTO.setMessageType("0800");
-        //iso8583DTO.setSystemTraceAuditNumber_11("198124");
-        //iso8583DTO.setAcquiringInstitutionIdentificationCode_32("00008600005");
-        //iso8583DTO.setCardAcceptorTerminalIdentification_41("00018644");
-        //iso8583DTO.setCardAcceptorIdentificationCode_42("852999958120501");
-        //iso8583DTO.setReservedPrivate_60("50000001003");
-        //iso8583DTO.setReservedPrivate_63("001");
-        //
-        //
-        //String sendMsg;
-        //try {
-        //     //组包
-        //    sendMsg = "6006090000"+"800100000000"+NumberStringUtil.str2HexStr("8529999581205010001864400000000860000500000000")
-        //            +NumberStringUtil.str2HexStr("852999958120501")+ISO8583Util.packISO8583DTO(iso8583DTO);
-        //    String strHex2 = String.format("%04x",sendMsg.length()/2);
-        //    sendMsg = strHex2 + sendMsg;
-        //    System.out.println(" ===  sendMsg  ====   "+sendMsg);
-        //    String result = ISO8583Util.send8583(sendMsg,"58.248.241.169",10089);
-        //
-        //    System.out.println(" ====  result  ===   "+result);
-        //     //解包
-        //    ISO8583DTO iso8583DTO1281 = ISO8583Util.unpackISO8583DTO(result);
-        //    System.out.println(iso8583DTO1281.toString());
-        //} catch (Exception e) {
-        //    System.out.println(e);
-        //}
+        ISO8583DTO iso8583DTO = new ISO8583DTO();
+        iso8583DTO.setMessageType("0800");
+        iso8583DTO.setSystemTraceAuditNumber_11("198124");
+        iso8583DTO.setAcquiringInstitutionIdentificationCode_32("00008600005");
+        iso8583DTO.setCardAcceptorTerminalIdentification_41("00018644");
+        iso8583DTO.setCardAcceptorIdentificationCode_42("852999958120501");
+        iso8583DTO.setReservedPrivate_60("50000001003");
+        iso8583DTO.setReservedPrivate_63("001");
+
+
+        String sendMsg;
+        try {
+             //组包
+            sendMsg = "6006090000"+"800100000000"+NumberStringUtil.str2HexStr("8529999581205010001864400000000860000500000000")
+                    +NumberStringUtil.str2HexStr("852999958120501")+ISO8583Util.packISO8583DTO(iso8583DTO);
+            String strHex2 = String.format("%04x",sendMsg.length()/2);
+            sendMsg = strHex2 + sendMsg;
+            System.out.println(" ===  sendMsg  ====   "+sendMsg);
+            String result = ISO8583Util.send8583(sendMsg,"58.248.241.169",10089);
+
+            System.out.println(" ====  result  ===   "+result);
+             //解包
+            ISO8583DTO iso8583DTO1281 = ISO8583Util.unpackISO8583DTO(result);
+            System.out.println(iso8583DTO1281.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 
