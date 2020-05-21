1ccc49e51312658392b5e7ef883f0808c9aa8f40
yangxu <yangxu@asianwallets.com>
2020-05-21 10:26:43 +0800

    1

--- asianwallets-common/src/main/java/com/asianwallets/common/dto/th/tlv/TlvDemo.java
+++ asianwallets-common/src/main/java/com/asianwallets/common/dto/th/tlv/TlvDemo.java
@@ -14,31 +14,11 @@ import java.util.List;
 public class TlvDemo {
 
     public static void main(String[] args) throws Exception{
-        ////测试数据
-        //String data = "5F5221303002020232303139303631333030303030313130353230303035323830310202";
-        //
-        //TLVTools  tlvTools = new TLVTools();
-        //TLV tlv;
-        //tlvTools.unpack(data);
-        //
-        //for(int i=0;i<tlvTools.tlvList.size();i++)
-        //{
-        //    tlv = tlvTools.tlvList.get(i);
-        //    System.out.print("   标签:["+i+"]"+"\n");
-        //    System.out.print("  tag:"+tlv.getTag()+"\n");
-        //    System.out.print("  len:"+tlv.getLen()+"\n");
-        //    System.out.print("value:"+ tlvTools.bytesToHex(tlv.getValue())+"\n");
-        //}
 
         System.out.println("----------------------------- 组码 ------------------------------------------------");
         BerTlvBuilder berTlvBuilder = new BerTlvBuilder();
         //这里的Tag要用16进制,Length是自动算出来的,最后是要存的数据
         berTlvBuilder.addHex(new BerTag(0x5F52),"303002020232303139303631333030303030313130353230303035323830310202");
-        //5F5109BDBBD2D7B3C9B9A6025F55143030303030320231393831303002303531330202
-        //berTlvBuilder.addHex(new BerTag(0x51),"BDBBD2D7B3C9B9A602");
-        //berTlvBuilder.addHex(new BerTag(0x55),"3030303030320231393831303002303531330202");
-        ////add value的时候可以选择是text格式或者是data byte[]等等其他都可以
-        //berTlvBuilder.addDate(new BerTag(0x3), new Date());
         ////默认是不支持汉字的，所以我们要把他转成字节
         //byte[] bytes3 = "我爱java，java爱我".getBytes("UTF-8");
         //berTlvBuilder.addBytes(new BerTag(0x4),bytes3);
