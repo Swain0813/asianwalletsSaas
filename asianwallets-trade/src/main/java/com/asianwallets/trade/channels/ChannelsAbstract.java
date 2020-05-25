f1503aee6ee09360ea54a61d33f49c752d4ff319
DESKTOP-311TI2U\yangs <yangshanlong@asianwallets.com>
2020-05-25 17:03:46 +0800

    11

--- asianwallets-trade/src/main/java/com/asianwallets/trade/channels/ChannelsAbstract.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/channels/ChannelsAbstract.java
@@ -65,8 +65,9 @@ public abstract class ChannelsAbstract {
      * @return 通用响应实体
      */
     public abstract BaseResponse cancel(Channel channel,OrderRefund orderRefund, RabbitMassage rabbitMassage);
+    
     /**
-     * 撤销方法
+     * 冲正方法
      *
      * @param channel      冲正
      * @return 通用响应实体
