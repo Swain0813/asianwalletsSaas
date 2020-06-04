87a501da56f66339d4059779aaf9b131aed18c68
yangxu <yangxu@asianwallets.com>
2020-06-04 14:47:58 +0800

    1

--- asianwallets-trade/src/main/java/com/asianwallets/trade/channels/upi/impl/UpiserviceImpl.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/channels/upi/impl/UpiserviceImpl.java
@@ -1,6 +1,12 @@
 package com.asianwallets.trade.channels.upi.impl;
 
+import com.asianwallets.common.constant.TradeConstant;
+import com.asianwallets.common.entity.Channel;
+import com.asianwallets.common.entity.Orders;
+import com.asianwallets.common.response.BaseResponse;
+import com.asianwallets.trade.channels.ChannelsAbstractAdapter;
 import com.asianwallets.trade.channels.upi.Upiservice;
+import com.asianwallets.trade.utils.HandlerType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +17,12 @@ import org.springframework.stereotype.Service;
  **/
 @Slf4j
 @Service
-public class UpiserviceImpl implements Upiservice {
+@HandlerType(TradeConstant.UPI)
+public class UpiserviceImpl extends ChannelsAbstractAdapter implements Upiservice {
 
 
+    @Override
+    public BaseResponse offlineCSB(Orders orders, Channel channel) {
+        return null;
+    }
 }
