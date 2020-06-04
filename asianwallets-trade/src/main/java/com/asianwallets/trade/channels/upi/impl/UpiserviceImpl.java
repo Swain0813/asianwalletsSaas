1b77be989b9d754eaf2a954706c275dd2bac3b4a
yangxu <yangxu@asianwallets.com>
2020-06-04 14:59:55 +0800

    1

--- asianwallets-trade/src/main/java/com/asianwallets/trade/channels/upi/impl/UpiserviceImpl.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/channels/upi/impl/UpiserviceImpl.java
@@ -1,14 +1,24 @@
 package com.asianwallets.trade.channels.upi.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.asianwallets.common.constant.TradeConstant;
+import com.asianwallets.common.dto.upi.UpiDTO;
+import com.asianwallets.common.dto.upi.UpiPayDTO;
 import com.asianwallets.common.entity.Channel;
 import com.asianwallets.common.entity.Orders;
+import com.asianwallets.common.exception.BusinessException;
 import com.asianwallets.common.response.BaseResponse;
+import com.asianwallets.common.response.EResultEnum;
 import com.asianwallets.trade.channels.ChannelsAbstractAdapter;
 import com.asianwallets.trade.channels.upi.Upiservice;
+import com.asianwallets.trade.feign.ChannelsFeign;
 import com.asianwallets.trade.utils.HandlerType;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * @description:
@@ -20,9 +30,60 @@ import org.springframework.stereotype.Service;
 @HandlerType(TradeConstant.UPI)
 public class UpiserviceImpl extends ChannelsAbstractAdapter implements Upiservice {
 
+    @Autowired
+    private ChannelsFeign channelsFeign;
 
     @Override
     public BaseResponse offlineCSB(Orders orders, Channel channel) {
+
+        UpiDTO upiDTO = new UpiDTO();
+        upiDTO.setChannel(channel);
+        UpiPayDTO upiPayDTO = this.createCSBDTO(orders,channel);
+        upiDTO.setUpiPayDTO(upiPayDTO);
+        log.info("==================【UPI线下CSB】==================【调用Channels服务】【QfPay-CSB接口】  upiDTO: {}", JSON.toJSONString(upiDTO));
+        BaseResponse channelResponse = channelsFeign.upiPay(upiDTO);
+        log.info("==================【UPI线下CSB】==================【调用Channels服务】【QfPay-CSB接口】  channelResponse: {}", JSON.toJSONString(channelResponse));
+        //请求失败
+        if (!TradeConstant.HTTP_SUCCESS.equals(channelResponse.getCode())) {
+            log.info("==================【UPI线下CSB】==================【调用Channels服务】【QfPay-CSB接口】-【请求状态码异常】");
+            throw new BusinessException(EResultEnum.ORDER_CREATION_FAILED.getCode());
+        }
+
         return null;
+    }
+
+    /**
+     * @Author YangXu
+     * @Date 2020/6/4
+     * @Descripate 创建CSBDTO
+     * @return
+     **/
+    private UpiPayDTO createCSBDTO(Orders orders, Channel channel) {
+        UpiPayDTO upiPayDTO = new UpiPayDTO();
+        upiPayDTO.setVersion("2.0.0");
+        upiPayDTO.setTrade_code("PAY");
+        // BACKSTAGEALIPAY 银行直连参数 UNIONZS：银联国际二维码主扫，BACKSTAGEUNION：银联国际二维码反扫
+        //主扫CSB 反扫BSC
+        upiPayDTO.setBank_code("UNIONZS");
+        upiPayDTO.setAgencyId(channel.getChannelMerchantId());
+        //upiPayDTO.setChild_merchant_no("574034451110001");
+        upiPayDTO.setTerminal_no(channel.getExtend6());
+        upiPayDTO.setOrder_no(orders.getId());
+        upiPayDTO.setAmount(orders.getTradeAmount().toString());
+        upiPayDTO.setCurrency_type(orders.getTradeCurrency());
+        upiPayDTO.setSett_currency_type(orders.getTradeCurrency());
+        //upiPayDTO.setAuth_code("288009714923099525");
+        //upiPayDTO.setCvn2();
+        //upiPayDTO.setValid();
+        upiPayDTO.setProduct_name("Saving the world requires sacrifice.");
+        //upiPayDTO.setProduct_desc();
+        //upiPayDTO.setProduct_type();
+        //upiPayDTO.setUser_name();
+        //upiPayDTO.setUser_cert_type();
+        //upiPayDTO.setUser_cert_no();
+        upiPayDTO.setReturn_url("https://testpay.sicpay.com/");
+        upiPayDTO.setNotify_url("https://testpay.sicpay.com/");
+        upiPayDTO.setClient_ip("120.236.178.23");
+        return upiPayDTO;
     }
 }
