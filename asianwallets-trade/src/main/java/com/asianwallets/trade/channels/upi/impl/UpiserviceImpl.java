20366d360e96230aba7e72f2adaed5d2cdd92a2e
yangxu <yangxu@asianwallets.com>
2020-06-05 11:38:03 +0800

    1

--- asianwallets-trade/src/main/java/com/asianwallets/trade/channels/upi/impl/UpiserviceImpl.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/channels/upi/impl/UpiserviceImpl.java
@@ -2,31 +2,40 @@ package com.asianwallets.trade.channels.upi.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.asianwallets.common.constant.AD3MQConstant;
+import com.asianwallets.common.constant.AsianWalletConstant;
 import com.asianwallets.common.constant.TradeConstant;
 import com.asianwallets.common.dto.upi.UpiDTO;
 import com.asianwallets.common.dto.upi.UpiPayDTO;
 import com.asianwallets.common.dto.upi.utils.CryptoUtil;
 import com.asianwallets.common.entity.Channel;
 import com.asianwallets.common.entity.Orders;
+import com.asianwallets.common.entity.RabbitMassage;
 import com.asianwallets.common.exception.BusinessException;
 import com.asianwallets.common.response.BaseResponse;
 import com.asianwallets.common.response.EResultEnum;
+import com.asianwallets.common.vo.clearing.FundChangeDTO;
 import com.asianwallets.trade.channels.ChannelsAbstractAdapter;
 import com.asianwallets.trade.channels.upi.Upiservice;
 import com.asianwallets.trade.config.AD3ParamsConfig;
+import com.asianwallets.trade.dao.ChannelsOrderMapper;
+import com.asianwallets.trade.dao.OrdersMapper;
 import com.asianwallets.trade.feign.ChannelsFeign;
+import com.asianwallets.trade.rabbitmq.RabbitMQSender;
+import com.asianwallets.trade.service.ClearingService;
+import com.asianwallets.trade.service.CommonBusinessService;
+import com.asianwallets.trade.service.CommonRedisDataService;
 import com.asianwallets.trade.utils.HandlerType;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.lang.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import tk.mybatis.mapper.entity.Example;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Date;
-import java.util.Map;
 
 /**
  * @description:
@@ -43,6 +52,18 @@ public class UpiserviceImpl extends ChannelsAbstractAdapter implements Upiservic
     @Autowired
     @Qualifier(value = "ad3ParamsConfig")
     private AD3ParamsConfig ad3ParamsConfig;
+    @Autowired
+    private OrdersMapper ordersMapper;
+    @Autowired
+    private ChannelsOrderMapper channelsOrderMapper;
+    @Autowired
+    private CommonBusinessService commonBusinessService;
+    @Autowired
+    private CommonRedisDataService commonRedisDataService;
+    @Autowired
+    private RabbitMQSender rabbitMQSender;
+    @Autowired
+    private ClearingService clearingService;
 
     @Override
     public BaseResponse offlineCSB(Orders orders, Channel channel) {
@@ -99,18 +120,120 @@ public class UpiserviceImpl extends ChannelsAbstractAdapter implements Upiservic
      **/
     @Override
     public String upiServerCallback(JSONObject jsonObject) {
+        JSONObject json;
         try {
             final PublicKey yhPubKey = CryptoUtil.getRSAPublicKeyByFileSuffix(this.getClass().getResource(ad3ParamsConfig.getUpiPublicKeyPath()).getPath(), "pem", "RSA");
             final PrivateKey hzfPriKey = CryptoUtil.getRSAPrivateKeyByFileSuffix(this.getClass().getResource(ad3ParamsConfig.getUpiPrivateKeyPath()).getPath(), "pem", null, "RSA");
-            String result =CryptoUtil.respDecryption(jsonObject, hzfPriKey, yhPubKey);
+
+
+            String result = CryptoUtil.respDecryption(jsonObject, hzfPriKey, yhPubKey);
             log.info("===============【upi回调】===============【返回】 result: {}", result);
-            JSONObject json = (JSONObject) JSONObject.parse(result);
-
-
-
+            json = (JSONObject) JSONObject.parse(result);
         } catch (Exception e) {
             log.info("================【upi回调】================【异常】", e);
+            return "success";
         }
+        //查询订单信息
+        Orders orders = ordersMapper.selectByPrimaryKey(json.get("order_no").toString());
+        if (orders == null) {
+            log.info("==================【upi回调】==================【订单为空】");
+            return "success";
+        }
+        //校验订单状态
+        if (!TradeConstant.ORDER_PAYING.equals(orders.getTradeStatus())) {
+            log.info("=================【upi回调】=================【订单状态不为支付中】");
+            return "success";
+        }
+        //通道回调时间
+        orders.setChannelCallbackTime(new Date());
+        //修改时间
+        orders.setUpdateTime(new Date());
+        Example example = new Example(Orders.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("tradeStatus", "2");
+        criteria.andEqualTo("id", orders.getId());
+        if ("1".equals(json.get("pay_result").toString())) {
+            //支付成功
+            log.info("=================【upi回调】=================【订单已支付成功】 orderId: {}", orders.getId());
+            //未发货
+            orders.setDeliveryStatus(TradeConstant.UNSHIPPED);
+            //未签收
+            orders.setReceivedStatus(TradeConstant.NO_RECEIVED);
+            orders.setTradeStatus((TradeConstant.ORDER_PAY_SUCCESS));
+            orders.setChannelNumber(json.get("pay_no").toString());
+            try {
+                channelsOrderMapper.updateStatusById(orders.getId(), orders.getChannelNumber(), TradeConstant.TRADE_SUCCESS);
+            } catch (Exception e) {
+                log.error("=================【upi回调】=================【更新通道订单异常】", e);
+            }
+            //更新订单信息
+            if (ordersMapper.updateByExampleSelective(orders, example) == 1) {
+                log.info("=================【upi回调】=================【订单支付成功后更新数据库成功】 orderId: {}", orders.getId());
+                //计算支付成功时的通道网关手续费
+                commonBusinessService.calcCallBackGatewayFeeSuccess(orders);
+                //TODO 添加日交易限额与日交易笔数
+                //commonBusinessService.quota(orders.getMerchantId(), orders.getProductCode(), orders.getTradeAmount());
+                //支付成功后向用户发送邮件
+                commonBusinessService.sendEmail(orders);
+                try {
+                    //账户信息不存在的场合创建对应的账户信息
+                    if (commonRedisDataService.getAccountByMerchantIdAndCurrency(orders.getMerchantId(), orders.getOrderCurrency()) == null) {
+                        log.info("=================【upi回调】=================【上报清结算前线下下单创建账户信息】");
+                        commonBusinessService.createAccount(orders);
+                    }
+                    //分润
+                    if (!StringUtils.isEmpty(orders.getAgentCode()) || !StringUtils.isEmpty(orders.getRemark8())) {
+                        rabbitMQSender.send(AD3MQConstant.SAAS_FR_DL, orders.getId());
+                    }
+                    FundChangeDTO fundChangeDTO = new FundChangeDTO(orders, TradeConstant.NT);
+                    //上报清结算资金变动接口
+                    BaseResponse fundChangeResponse = clearingService.fundChange(fundChangeDTO);
+                    if (fundChangeResponse.getCode().equals(TradeConstant.CLEARING_FAIL)) {
+                        log.info("=================【upi回调】=================【上报清结算失败,上报队列】 【MQ_PLACE_ORDER_FUND_CHANGE_FAIL】");
+                        com.asianwallets.common.entity.RabbitMassage rabbitMassage = new com.asianwallets.common.entity.RabbitMassage(AsianWalletConstant.THREE, JSON.toJSONString(orders));
+                        rabbitMQSender.send(AD3MQConstant.MQ_PLACE_ORDER_FUND_CHANGE_FAIL, JSON.toJSONString(rabbitMassage));
+                    }
+                } catch (Exception e) {
+                    log.error("=================【upi回调】=================【上报清结算异常,上报队列】 【MQ_PLACE_ORDER_FUND_CHANGE_FAIL】", e);
+                    RabbitMassage rabbitMassage = new com.asianwallets.common.entity.RabbitMassage(AsianWalletConstant.THREE, JSON.toJSONString(orders));
+                    rabbitMQSender.send(AD3MQConstant.MQ_PLACE_ORDER_FUND_CHANGE_FAIL, JSON.toJSONString(rabbitMassage));
+                }
+            } else {
+                log.info("=================【upi回调】=================【订单支付成功后更新数据库失败】 orderId: {}", orders.getId());
+            }
+
+        } else if ("2".equals(json.get("pay_result").toString())) {
+            //支付失败
+            log.info("=================【upi回调】=================【订单已支付失败】 orderId: {}", orders.getId());
+            orders.setTradeStatus(TradeConstant.ORDER_PAY_FAILD);
+            orders.setRemark5(json.get("resp_desc").toString());
+            try {
+                channelsOrderMapper.updateStatusById(orders.getId(), json.get("pay_no").toString(), TradeConstant.TRADE_FALID);
+            } catch (Exception e) {
+                log.error("=================【upi回调】=================【更新通道订单异常】", e);
+            }
+            //计算支付失败时的通道网关手续费
+            commonBusinessService.calcCallBackGatewayFeeFailed(orders);
+            if (ordersMapper.updateByExampleSelective(orders, example) == 1) {
+                log.info("=================【upi回调】=================【订单支付失败后更新数据库成功】 orderId: {}", orders.getId());
+            } else {
+                log.info("=================【upi回调】=================【订单支付失败后更新数据库失败】 orderId: {}", orders.getId());
+            }
+
+        } else {
+            log.info("=================【upi回调】=================【订单是交易中】 orderId: {}", orders.getId());
+            return "success";
+        }
+
+        try {
+            //商户服务器回调地址不为空,回调商户服务器
+            if (!StringUtils.isEmpty(orders.getServerUrl())) {
+                commonBusinessService.replyReturnUrl(orders);
+            }
+        } catch (Exception e) {
+            log.error("=================【QfPay服务器回调】=================【回调商户异常】", e);
+        }
+
 
         return "success";
     }
