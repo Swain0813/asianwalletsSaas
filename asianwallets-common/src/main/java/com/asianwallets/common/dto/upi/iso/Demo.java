416b4799cf72b372ba96f923dcfa20ebdf4c8eda
yangxu <yangxu@asianwallets.com>
2020-06-10 16:41:04 +0800

    1

--- asianwallets-common/src/main/java/com/asianwallets/common/dto/upi/iso/Demo.java
+++ asianwallets-common/src/main/java/com/asianwallets/common/dto/upi/iso/Demo.java
@@ -23,10 +23,10 @@ public class Demo {
     //        210.48.142.168 6000 走网控（主机）
     //        210.48.142.168 7000 走网控（备机）
     //TPDU NII:
-    //        006(测试环境)
-    //        007(生产环境)
+    //        006(测试环境)  6000060000
+    //        007(生产环境)  6000070000
     private static String ip = "210.48.142.168";
-    private static String port = "6000";
+    private static String port = "7000";
     private static String merchantId = "000000000003421";
     private static String terminalId = "00001903";
     private static String key = "861B7FBD78A6E196";
@@ -36,36 +36,34 @@ public class Demo {
 
         ISO8583DTO iso8583DTO = new ISO8583DTO();
         iso8583DTO.setMessageType("0200");
-        iso8583DTO.setProcessingCode_3("700206");//查询
-        //        iso8583DTO.setProcessingCode_3("700200");//主扫
-        //        iso8583DTO.setProcessingCode_3("400101");//被扫
+        iso8583DTO.setProcessingCode_2("6226090001008891");
+        iso8583DTO.setProcessingCode_3("280000");
         //交易金额
         iso8583DTO.setAmountOfTransactions_4("000000000100");
         //受卡方系统跟踪号
         iso8583DTO.setSystemTraceAuditNumber_11(domain11);
+        //iso8583DTO.setTimeOfLocalTransaction_12("155943");
+        //iso8583DTO.setDateOfLocalTransaction_13("0610");
         //服务点输入方式码
-        iso8583DTO.setPointOfServiceEntryMode_22("000");
+        iso8583DTO.setPointOfServiceEntryMode_22("022");
         //服务点条件码
-        iso8583DTO.setPointOfServiceConditionMode_25("00");
+        iso8583DTO.setPointOfServiceConditionMode_25("82");
         //受理方标识码 (机构号)
-        iso8583DTO.setAcquiringInstitutionIdentificationCode_32("08600005");
+        //iso8583DTO.setAcquiringInstitutionIdentificationCode_32("08600005");
         //受卡机终端标识码 (设备号)
         iso8583DTO.setCardAcceptorTerminalIdentification_41(terminalId);
         //受卡方标识码 (商户号)
         iso8583DTO.setCardAcceptorIdentificationCode_42(merchantId);
-        // iso8583DTO.setAdditionalData_46("5F5206303002300202");//主扫
-        iso8583DTO.setAdditionalData_46("5F5221303002020232303230303532303030303030313130363130303137383436350202");//查询
-        //        String scanCode = "134750495118463486";
-        //        iso8583DTO.setAdditionalData_46("5F52193030023002" + NumberStringUtil.str2HexStr(scanCode) + "0202");//被扫
         //交易货币代码
         iso8583DTO.setCurrencyCodeOfTransaction_49("344");
         //自定义域
-        iso8583DTO.setReservedPrivate_60("0100005A");//01000001000000000
+        iso8583DTO.setReservedPrivate_60("22000001000600");//01000001000000000
 
         //扫码组包
         String isoMsg = UpiIsoUtil.packISO8583DTO(iso8583DTO, key);
-        String sendMsg = "6000120000" +"602110200610"+ isoMsg;
+        String sendMsg = "6000060000" +"602110190121"+ isoMsg;
         System.out.println(" ===  扫码sendMsg  ====   " + sendMsg);
+
         Map<String, String> respMap = UpiIsoUtil.sendTCPRequest(ip, port, NumberStringUtil.str2Bcd(sendMsg));
         String result = respMap.get("respData");
         System.out.println(" ====  扫码result  ===   " + result);
