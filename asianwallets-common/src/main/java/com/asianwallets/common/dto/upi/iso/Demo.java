1b64e5fc291396e91f2e4cb8a9fe4726cbe4f5a6
yangxu <yangxu@asianwallets.com>
2020-06-10 17:26:15 +0800

    1

--- asianwallets-common/src/main/java/com/asianwallets/common/dto/upi/iso/Demo.java
+++ asianwallets-common/src/main/java/com/asianwallets/common/dto/upi/iso/Demo.java
@@ -35,36 +35,22 @@ public class Demo {
         String domain11 = IDS.uniqueID().toString().substring(0, 6);
 
         ISO8583DTO iso8583DTO = new ISO8583DTO();
-        iso8583DTO.setMessageType("0200");
-        iso8583DTO.setProcessingCode_2("6226090001008891");
-        iso8583DTO.setProcessingCode_3("280000");
-        //交易金额
-        iso8583DTO.setAmountOfTransactions_4("000000000100");
-        //受卡方系统跟踪号
+        iso8583DTO.setMessageType("0800");
         iso8583DTO.setSystemTraceAuditNumber_11(domain11);
-        //iso8583DTO.setTimeOfLocalTransaction_12("155943");
-        //iso8583DTO.setDateOfLocalTransaction_13("0610");
-        //服务点输入方式码
-        iso8583DTO.setPointOfServiceEntryMode_22("022");
-        //服务点条件码
-        iso8583DTO.setPointOfServiceConditionMode_25("82");
-        //受理方标识码 (机构号)
-        //iso8583DTO.setAcquiringInstitutionIdentificationCode_32("08600005");
         //受卡机终端标识码 (设备号)
         iso8583DTO.setCardAcceptorTerminalIdentification_41(terminalId);
         //受卡方标识码 (商户号)
         iso8583DTO.setCardAcceptorIdentificationCode_42(merchantId);
-        //交易货币代码
-        iso8583DTO.setCurrencyCodeOfTransaction_49("344");
         //自定义域
-        iso8583DTO.setReservedPrivate_60("22000001000600");//01000001000000000
-
+        iso8583DTO.setReservedPrivate_60("00000002003");//01000001000000000
+        iso8583DTO.setReservedPrivate_63("00");
         //扫码组包
-        String isoMsg = UpiIsoUtil.packISO8583DTO(iso8583DTO, key);
+        String isoMsg = UpiIsoUtil.packISO8583DTO(iso8583DTO, null);
         String sendMsg = "6000060000" +"602110190121"+ isoMsg;
         System.out.println(" ===  扫码sendMsg  ====   " + sendMsg);
 
-        Map<String, String> respMap = UpiIsoUtil.sendTCPRequest(ip, port, NumberStringUtil.str2Bcd(sendMsg));
+        Map<String, String> respMap = UpiIsoUtil.sendTCPRequest(ip, port, sendMsg.getBytes());
+        //Map<String, String> respMap = UpiIsoUtil.sendTCPRequest(ip, port, NumberStringUtil.str2Bcd(sendMsg));
         String result = respMap.get("respData");
         System.out.println(" ====  扫码result  ===   " + result);
         //解包
