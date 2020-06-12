bd929272f1fd2a6af4562ac91f7bb4cf3e5be8e1
yangxu <yangxu@asianwallets.com>
2020-06-12 17:54:31 +0800

    1

--- asianwallets-common/src/main/java/com/asianwallets/common/dto/upi/iso/Demo.java
+++ asianwallets-common/src/main/java/com/asianwallets/common/dto/upi/iso/Demo.java
@@ -1,13 +1,16 @@
 package com.asianwallets.common.dto.upi.iso;
 
 import com.alibaba.fastjson.JSON;
+import com.asianwallets.common.dto.th.ISO8583.EcbDesUtil;
 import com.asianwallets.common.dto.th.ISO8583.ISO8583DTO;
 import com.asianwallets.common.dto.th.ISO8583.ISO8583Util;
 import com.asianwallets.common.dto.th.ISO8583.NumberStringUtil;
+import com.asianwallets.common.utils.AESUtil;
 import com.asianwallets.common.utils.IDS;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @description:
@@ -30,9 +33,14 @@ public class Demo {
     private static String merchantId = "000000000003421";
     private static String terminalId = "00001903";
     private static String key_62 = "C80C9C5AEF671BB7AE63D50DDA0EE5FD1DD7DD2400E0A435AC12A51F50850E45DF0EF070195A1B1E3976E398D4F5A66F0E1A6D8602E218491E186CB1";
-    private static String key = "861B7FBD78A6E196";
+    private static String key = "A48946D992A2797C";
 
     public static void main(String[] args) throws Exception {
+        //test1();
+        test2();
+    }
+
+    private static void test1() throws Exception  {
         String domain11 = IDS.uniqueID().toString().substring(0, 6);
 
         ISO8583DTO iso8583DTO = new ISO8583DTO();
@@ -43,11 +51,8 @@ public class Demo {
         //受卡方标识码 (商户号)
         iso8583DTO.setCardAcceptorIdentificationCode_42(merchantId);
         //自定义域
-        //iso8583DTO.setReservedPrivate_60("9600000240000");//01000001000000000
-        ////iso8583DTO.setReservedPrivate_62("9F0605DF000000039F220101");
-        iso8583DTO.setReservedPrivate_60("99000002003");//01000001000000000
-        iso8583DTO.setReservedPrivate_62("DF9981805A9559A5F684F3988224B4FC8C45934C20994D27DF777039BC0B9D2D32B097DC4DFD1908875D58EEFBFF44B10D12B8C7512CF94BFFEAF6DDE46CA47CE672DEB961E9360FD4CFC5CB95B1DE02E6E744A169C0F2F4658051A387AC8269EC0ABB38AAA21E80D8046781E4577EB53917C549EDA64A3C06C30691E948848FC72CF4689F0605DF000000039F220101");
-        iso8583DTO.setReservedPrivate_63("00185769819");
+        iso8583DTO.setReservedPrivate_60("00000002003");//01000001000000000
+        iso8583DTO.setReservedPrivate_63("000");
         //扫码组包
         String isoMsg = UpiIsoUtil.packISO8583DTO(iso8583DTO, null);
         String sendMsg = "6000060000" +"601410190121"+ isoMsg;
@@ -58,10 +63,28 @@ public class Demo {
         //Map<String, String> respMap = UpiIsoUtil.sendTCPRequest(ip, port, sendMsg.getBytes());
         Map<String, String> respMap = UpiIsoUtil.sendTCPRequest(ip, port, NumberStringUtil.str2Bcd(sendMsg));
         String result = respMap.get("respData");
-
         System.out.println(" ====  扫码result  ===   " + result);
         //解包
         ISO8583DTO iso8583DTO1281 = UpiIsoUtil.unpackISO8583DTO(result);
         System.out.println("扫码结果:" + JSON.toJSONString(iso8583DTO1281));
+    }
+
+    private static void test2() {
+        String substring = key_62.substring(40, 56);
+        String trk = Objects.requireNonNull(EcbDesUtil.decode3DEA("3104BAC458BA1513043E4010FD642619", substring)).toUpperCase();
+        System.out.println(trk);
+    }
+    private static String trkEncryption(String str, String key) {
+        //80-112 Trk密钥位
+        String substring = key.substring(80, 112);
+        String trk = Objects.requireNonNull(EcbDesUtil.decode3DEA("3104BAC458BA1513043E4010FD642619", substring)).toUpperCase();
+        String newStr;
+        if (str.length() % 2 != 0) {
+            newStr = str.length() + str + "0";
+        } else {
+            newStr = str.length() + str;
+        }
+        byte[] bcd = NumberStringUtil.str2Bcd(newStr);
+        return Objects.requireNonNull(EcbDesUtil.encode3DEA(trk, cn.hutool.core.util.HexUtil.encodeHexStr(bcd))).toUpperCase();
     }
 }
