455b976056d12edb1bcc16660db316e4e214875a
XinRanShen <shenxinr@qq.com>
2020-06-16 16:28:56 +0800

    feat(aaa):

--- asianwallets-trade/src/main/java/com/asianwallets/trade/channels/th/impl/ThServiceImpl.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/channels/th/impl/ThServiceImpl.java
@@ -967,7 +967,7 @@ public class ThServiceImpl extends ChannelsAbstractAdapter implements ThService 
         String formatAmount = String.format("%012d", tradeAmount);
         iso8583DTO.setAmountOfTransactions_4(formatAmount);
         //受卡方系统跟踪号
-        iso8583DTO.setSystemTraceAuditNumber_11(String.valueOf(System.currentTimeMillis()).substring(0, 6));
+        iso8583DTO.setSystemTraceAuditNumber_11(orders.getId().substring(9, 15));
         //服务点输入方式码
         iso8583DTO.setPointOfServiceEntryMode_22("021");
         //服务点条件码
