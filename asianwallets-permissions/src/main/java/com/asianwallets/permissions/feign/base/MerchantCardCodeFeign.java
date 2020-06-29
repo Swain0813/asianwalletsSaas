a75e5849135c5b00589b7f0c1360ec5a7284b26a
DESKTOP-311TI2U\yangs <yangshanlong@asianwallets.com>
2020-06-29 11:00:10 +0800

    11

--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/MerchantCardCodeFeign.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/MerchantCardCodeFeign.java
@@ -1,28 +1,21 @@
 package com.asianwallets.permissions.feign.base;
-import com.asianwallets.common.dto.PreOrdersDTO;
+import com.asianwallets.common.dto.MerchantCardCodeDTO;
 import com.asianwallets.common.response.BaseResponse;
-import com.asianwallets.common.vo.ExportPreOrdersVO;
-import com.asianwallets.permissions.feign.base.impl.PreOrdersFeignImpl;
+import com.asianwallets.permissions.feign.base.impl.MerchantCardCodeFeignImpl;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.List;
+@FeignClient(value = "asianwallets-base", fallback = MerchantCardCodeFeignImpl.class)
+public interface MerchantCardCodeFeign {
 
-@FeignClient(value = "asianwallets-base", fallback = PreOrdersFeignImpl.class)
-public interface PreOrdersFeign {
+    @ApiOperation(value = "分页查询商户码牌信息信息")
+    @PostMapping("/cardCode/pageFindMerchantCardCode")
+    BaseResponse pageFindPreOrders(@RequestBody @ApiParam MerchantCardCodeDTO merchantCardCodeDTO);
 
-    @ApiOperation(value = "分页查询预授权订单信息")
-    @PostMapping("/preOrders/pageFindPreOrders")
-    BaseResponse pageFindPreOrders(@RequestBody @ApiParam PreOrdersDTO preOrdersDTO);
-
-    @ApiOperation(value = "查询预授权订单详情信息")
-    @PostMapping("/preOrders/getPreOrdersDetail")
-    BaseResponse getPreOrdersDetail(@RequestBody @ApiParam PreOrdersDTO preOrdersDTO);
-
-    @ApiOperation(value = "预授权订单导出")
-    @PostMapping("/preOrders/exportPreOrders")
-    List<ExportPreOrdersVO> exportPreOrders(@RequestBody @ApiParam PreOrdersDTO preOrdersDTO);
+    @ApiOperation(value = "查询商户码牌详情信息")
+    @PostMapping("/cardCode/getMerchantCardCode")
+    BaseResponse getMerchantCardCode(@RequestBody @ApiParam MerchantCardCodeDTO merchantCardCodeDTO);
 }
--- asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/impl/MerchantCardCodeFeignImpl.java
+++ asianwallets-permissions/src/main/java/com/asianwallets/permissions/feign/base/impl/MerchantCardCodeFeignImpl.java
@@ -1,4 +1,33 @@
 package com.asianwallets.permissions.feign.base.impl;
+import com.asianwallets.common.dto.MerchantCardCodeDTO;
+import com.asianwallets.common.exception.BusinessException;
+import com.asianwallets.common.response.BaseResponse;
+import com.asianwallets.common.response.EResultEnum;
+import com.asianwallets.permissions.feign.base.MerchantCardCodeFeign;
+import org.springframework.stereotype.Component;
 
-public class MerchantCardCodeFeignImpl {
+/**
+ * 商户码牌的实现类
+ */
+@Component
+public class MerchantCardCodeFeignImpl implements MerchantCardCodeFeign {
+    /**
+     * 分页查询商户码牌信息信息
+     * @param merchantCardCodeDTO
+     * @return
+     */
+    @Override
+    public BaseResponse pageFindPreOrders(MerchantCardCodeDTO merchantCardCodeDTO) {
+        throw new BusinessException(EResultEnum.ERROR.getCode());
+    }
+
+    /**
+     * 查询商户码牌详情信息
+     * @param merchantCardCodeDTO
+     * @return
+     */
+    @Override
+    public BaseResponse getMerchantCardCode(MerchantCardCodeDTO merchantCardCodeDTO) {
+        throw new BusinessException(EResultEnum.ERROR.getCode());
+    }
 }
