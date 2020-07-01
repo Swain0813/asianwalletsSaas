1b05292d1691da9961b3158d0f321a7a31743594
DESKTOP-311TI2U\yangs <yangshanlong@asianwallets.com>
2020-07-01 09:39:15 +0800

    修改

--- asianwallets-base/src/main/java/com/asianwallets/base/controller/QrCodeForwardController.java
+++ asianwallets-base/src/main/java/com/asianwallets/base/controller/QrCodeForwardController.java
@@ -2,6 +2,7 @@ package com.asianwallets.base.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +13,7 @@ import java.io.IOException;
 @RestController
 @Api(description = "静态码跳转功能")
 @RequestMapping("/qr")
+@Slf4j
 public class QrCodeForwardController {
     /**
      * 跳转到前端url
@@ -24,6 +26,7 @@ public class QrCodeForwardController {
     @CrossOrigin
     public void forward(@RequestParam @ApiParam String id, HttpServletResponse response, HttpServletRequest request) {
         String userAgent = request.getHeader("user-agent");
+        log.info("*******调用静态码支付跳转的输入金额的页面带过去的输入参数**************** 静态码编号:{},扫码的标志:{}",id,userAgent);
         ModelAndView mv=new ModelAndView();
         mv.addObject("id",id);
         mv.addObject("userAgent",userAgent);
