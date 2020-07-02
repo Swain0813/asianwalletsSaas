ee7ce3ae4553efef2005ae84857331c620316502
DESKTOP-311TI2U\yangs <yangshanlong@asianwallets.com>
2020-07-02 11:47:04 +0800

    11

--- asianwallets-trade/src/main/java/com/asianwallets/trade/service/impl/CommonRedisDataServiceImpl.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/service/impl/CommonRedisDataServiceImpl.java
@@ -493,8 +493,12 @@ public class CommonRedisDataServiceImpl implements CommonRedisDataService {
         if (merchantCardCode == null) {
             merchantCardCode = merchantCardCodeMapper.selectById(id);
             if (merchantCardCode == null) {
-                log.info("==================【根据码牌id查询商户码牌信息】==================【码牌信息对象不存在】 id: {}", id);
+                log.info("================交易服务==【根据码牌id查询商户码牌信息】==================【码牌信息对象不存在】 id: {}", id);
                 throw new BusinessException(EResultEnum.CODE_CARD_INFORMATION_DOES_NOT_EXIST.getCode());
+            }
+            if(!merchantCardCode.getEnabled()){
+                log.info("****************交易服务*******静态码信息已被禁用**************************");
+                throw new BusinessException(EResultEnum.MERCHANT_CARD_CODE_IS_ENABLE.getCode());
             }
             redisService.set(AsianWalletConstant.MERCHANT_CARD_CODE.concat("_").concat(merchantCardCode.getId()), JSON.toJSONString(merchantCardCode));
         }
