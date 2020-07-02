1b5a42cc9ad3ceda657ac47a98898b4f1da193bd
XinRanShen <shenxinr@qq.com>
2020-07-02 16:22:09 +0800

    feat(aa):

--- asianwallets-trade/src/main/java/com/asianwallets/trade/dto/OfflineCodeTradeDTO.java
+++ asianwallets-trade/src/main/java/com/asianwallets/trade/dto/OfflineCodeTradeDTO.java
@@ -20,46 +20,4 @@ public class OfflineCodeTradeDTO {
 
     @ApiModelProperty(value = "聚合码id")
     private String merchantCardCode;
-
-/*
-    //----------------【非必填参数】---------------------
-
-    @ApiModelProperty(value = "银行机构号")
-    private String issuerId;
-
-    @ApiModelProperty(value = "服务器回调地址")
-    private String serverUrl;
-
-    @ApiModelProperty(value = "浏览器回调地址")
-    private String browserUrl;
-
-    @ApiModelProperty(value = "商品名称")
-    private String productName;
-
-    @ApiModelProperty(value = "商品描述")
-    private String productDescription;
-
-    @ApiModelProperty(value = "付款人姓名")
-    private String payerName;
-
-    @ApiModelProperty(value = "付款人银行")
-    private String payerBank;
-
-    @ApiModelProperty(value = "付款人邮箱")
-    private String payerEmail;
-
-    @ApiModelProperty(value = "付款人电话")
-    private String payerPhone;
-
-    @ApiModelProperty(value = "语言")
-    private String language;
-
-    @ApiModelProperty(value = "remark1")
-    private String remark1;
-
-    @ApiModelProperty(value = "remark2")
-    private String remark2;
-
-    @ApiModelProperty(value = "remark3")
-    private String remark3;*/
 }
